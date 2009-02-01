cd0dfb0e5aa49b7e65fd9d6de403806eb3c2531e
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-02-01 04:13:41 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@14 9038e2a8-f00e-11dd-89c5-9df526a60542

---
+++ projects/text/binutils/src/com/googlecode/jwutils/binutils/HexExtract.java
@@ -0,0 +1,25 @@
+package com.googlecode.jwutils.binutils;
+
+/**
+ * A utility for extracting a region of a file, given a start pattern and an end
+ * pattern.<br/><br/>
+ * 
+ * With three arguments (a file, the start pattern, and the end pattern), the
+ * number of occurences of the start pattern and the end pattern will be printed.
+ * 
+ * @author Alexander Boyd
+ * 
+ */
+public class HexExtract
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
