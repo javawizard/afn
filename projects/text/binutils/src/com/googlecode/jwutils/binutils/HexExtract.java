faa64851a3be1e8ba24415bcc41e4aa0a12e841d
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-02-01 05:51:10 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@15 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/text/binutils/src/com/googlecode/jwutils/binutils/HexExtract.java
+++ projects/text/binutils/src/com/googlecode/jwutils/binutils/HexExtract.java
@@ -5,7 +5,12 @@ package com.googlecode.jwutils.binutils;
  * pattern.<br/><br/>
  * 
  * With three arguments (a file, the start pattern, and the end pattern), the
- * number of occurences of the start pattern and the end pattern will be printed.
+ * number of occurences of the start pattern and the end pattern will be
+ * printed. To actually extract, five arguments need to be given. The fourth is
+ * which of all of the instances of the second input that were found to use to
+ * start the block, and the fifth is which to use as the end. A sixth argument
+ * can additionally be given, which is a number of extra bytes to read after the
+ * end.
  * 
  * @author Alexander Boyd
  * 
