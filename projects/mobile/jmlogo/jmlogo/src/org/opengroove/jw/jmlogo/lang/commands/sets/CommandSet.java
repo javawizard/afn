a9df68602fff2fd871f3e7529fe9af2c96cecae7
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-02-10 01:06:53 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@74 9038e2a8-f00e-11dd-89c5-9df526a60542

---
+++ projects/mobile/jmlogo/jmlogo/src/org/opengroove/jw/jmlogo/lang/commands/sets/CommandSet.java
@@ -0,0 +1,8 @@
+package org.opengroove.jw.jmlogo.lang.commands.sets;
+
+import org.opengroove.jw.jmlogo.lang.Interpreter;
+
+public interface CommandSet
+{
+    public void installCommandSet(Interpreter interpreter);
+}
