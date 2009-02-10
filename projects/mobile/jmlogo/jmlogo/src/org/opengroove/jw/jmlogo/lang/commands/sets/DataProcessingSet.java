0d3371590f6bbebed4360a243fa6729d2115639f
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-02-10 07:14:05 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@84 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/mobile/jmlogo/jmlogo/src/org/opengroove/jw/jmlogo/lang/commands/sets/DataProcessingSet.java
+++ projects/mobile/jmlogo/jmlogo/src/org/opengroove/jw/jmlogo/lang/commands/sets/DataProcessingSet.java
@@ -1,8 +1,21 @@
 package org.opengroove.jw.jmlogo.lang.commands.sets;
 
 import org.opengroove.jw.jmlogo.lang.Command;
+import org.opengroove.jw.jmlogo.lang.InterpreterContext;
+import org.opengroove.jw.jmlogo.lang.ListToken;
+import org.opengroove.jw.jmlogo.lang.NamedCommand;
+import org.opengroove.jw.jmlogo.lang.Token;
 
 public class DataProcessingSet
 {
-    public static final Command[] set = new Command[] {};
+    public static final Command[] set =
+        new Command[] { new NamedCommand("list", 2, 256)
+        {
+            
+            public Token run(InterpreterContext context, Token[] arguments)
+            {
+                ListToken token = new ListToken(arguments);
+                return token;
+            }
+        } };
 }
