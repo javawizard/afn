c42634ea0eb8cf9afc2dddaf7214aff7c8aaeac5
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-02-11 20:25:03 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@111 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/mobile/jmlogo/jmlogo/src/org/opengroove/jw/jmlogo/lang/commands/sets/LogicSet.java
+++ projects/mobile/jmlogo/jmlogo/src/org/opengroove/jw/jmlogo/lang/commands/sets/LogicSet.java
@@ -1,8 +1,21 @@
 package org.opengroove.jw.jmlogo.lang.commands.sets;
 
 import org.opengroove.jw.jmlogo.lang.Command;
+import org.opengroove.jw.jmlogo.lang.InterpreterContext;
+import org.opengroove.jw.jmlogo.lang.NamedCommand;
+import org.opengroove.jw.jmlogo.lang.Token;
+import org.opengroove.jw.jmlogo.lang.WordToken;
 
 public class LogicSet
 {
-    public static final Command[] set = new Command[] {new NamedCommand()};
+    public static final Command[] set = new Command[] { new NamedCommand("not", 1, 1)
+    {
+        
+        public Token run(InterpreterContext context, Token[] arguments)
+        {
+            validateWord(arguments[0]);
+            WordToken word = (WordToken) arguments[0];
+            return new WordToken(!word.getBool());
+        }
+    } };
 }
