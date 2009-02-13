6318431a8df0adae4b0109f338455b919a08f890
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-02-13 07:11:37 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@123 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/mobile/jmlogo/jmlogo/src/org/opengroove/jw/jmlogo/lang/commands/sets/MathSet.java
+++ projects/mobile/jmlogo/jmlogo/src/org/opengroove/jw/jmlogo/lang/commands/sets/MathSet.java
@@ -1,120 +1,174 @@
 package org.opengroove.jw.jmlogo.lang.commands.sets;
 
+import java.util.Vector;
+
+import henson.midp.Float11;
+
 import org.opengroove.jw.jmlogo.lang.Command;
-import org.opengroove.jw.jmlogo.lang.InterpreterContext;
-import org.opengroove.jw.jmlogo.lang.NamedCommand;
-import org.opengroove.jw.jmlogo.lang.Token;
-import org.opengroove.jw.jmlogo.lang.WordToken;
 import org.opengroove.jw.jmlogo.lang.commands.math.OneArgMathCommand;
 import org.opengroove.jw.jmlogo.lang.commands.math.TwoArgMathCommand;
 import org.opengroove.jw.jmlogo.utils.Math2;
 
-import henson.midp.*;
-
 public class MathSet
 {
-    public static Command[] commands = new Command[] { new OneArgMathCommand()
+    public static Command[] commands = loadCommands();
+    
+    private static Command[] loadCommands()
     {
+        Vector list = new Vector();
         
-        public double compute(double v)
+        list.addElement(new OneArgMathCommand()
         {
-            return Math.sqrt(v);
-        }
-        
-        public String getName()
+            public double compute(double v)
+            {
+                return Math.sqrt(v);
+            }
+            
+            public String getName()
+            {
+                return "sqrt";
+            }
+        });
+        list.addElement(new OneArgMathCommand()
         {
-            return "sqrt";
-        }
-    }, new OneArgMathCommand()
-    {
-        
-        public double compute(double v)
+            public double compute(double v)
+            {
+                return Math2.round(v);
+            }
+            
+            public String getName()
+            {
+                return "round";
+            }
+        });
+        list.addElement(new OneArgMathCommand()
         {
-            return Math2.round(v);
-        }
-        
-        public String getName()
+            public double compute(double v)
+            {
+                if (v > 0)
+                    return 1;
+                else if (v < 0)
+                    return -1;
+                else
+                    return 0;
+            }
+            
+            public String getName()
+            {
+                return "sign";
+            }
+        });
+        list.addElement(new TwoArgMathCommand()
         {
-            return "round";
-        }
-    }, new OneArgMathCommand()
-    {
-        
-        public double compute(double v)
+            
+            public double compute(double v1, double v2)
+            {
+                return Float11.pow(v1, v2);
+            }
+            
+            public String getName()
+            {
+                return "power";
+            }
+        });
+        list.addElement(new OneArgMathCommand()
         {
-            return 0;
-        }
-        
-        public String getName()
+            
+            public double compute(double v)
+            {
+                return Math.sin(Math.toRadians(v));
+            }
+            
+            public String getName()
+            {
+                return "sin";
+            }
+        });
+        list.addElement(new OneArgMathCommand()
         {
-            return "sign";
-        }
-    }, new OneArgMathCommand()
-    {
-        
-        public double compute(double v)
+            
+            public double compute(double v)
+            {
+                return Math.toDegrees(Float11.asin(v));
+            }
+            
+            public String getName()
+            {
+                // TODO Auto-generated method stub
+                return "arcsin";
+            }
+        });
+        list.addElement(new OneArgMathCommand()
         {
-            return Math.sin(Math.toRadians(v));
-        }
-        
-        public String getName()
+            
+            public double compute(double v)
+            {
+                return Math.tan(Math.toRadians(v));
+            }
+            
+            public String getName()
+            {
+                // TODO Auto-generated method stub
+                return "tan";
+            }
+        });
+        list.addElement(new OneArgMathCommand()
         {
-            return "sin";
-        }
-    }, new OneArgMathCommand()
-    {
-        
-        public double compute(double v)
+            
+            public double compute(double v)
+            {
+                // TODO Auto-generated method stub
+                return Math.toDegrees(Float11.atan(v));
+            }
+            
+            public String getName()
+            {
+                // TODO Auto-generated method stub
+                return "arctan";
+            }
+        });
+        list.addElement(new TwoArgMathCommand()
         {
-            // TODO Auto-generated method stub
-            return 0;
-        }
-        
-        public String getName()
+            
+            public double compute(double v1, double v2)
+            {
+                return Math.toDegrees(Float11.atan2(v1, v2));
+            }
+            
+            public String getName()
+            {
+                // TODO Auto-generated method stub
+                return "arctan2";
+            }
+        });
+        list.addElement(new OneArgMathCommand()
         {
-            // TODO Auto-generated method stub
-            return "arcsin";
-        }
-    }, new OneArgMathCommand()
-    {
-        
-        public double compute(double v)
+            
+            public double compute(double v)
+            {
+                return Math.toDegrees(v);
+            }
+            
+            public String getName()
+            {
+                return "todegrees";
+            }
+        });
+        list.addElement(new OneArgMathCommand()
         {
-            // TODO Auto-generated method stub
-            return 0;
-        }
-        
-        public String getName()
-        {
-            // TODO Auto-generated method stub
-            return "tan";
-        }
-    }, new OneArgMathCommand()
-    {
-        
-        public double compute(double v)
-        {
-            // TODO Auto-generated method stub
-            return 0;
-        }
-        
-        public String getName()
-        {
-            // TODO Auto-generated method stub
-            return "arctan";
-        }
-    }, new TwoArgMathCommand()
-    {
-        
-        public double compute(double v1, double v2)
-        {
-            return Float11.atan2(v1, v2);
-        }
-        
-        public String getName()
-        {
-            // TODO Auto-generated method stub
-            return "arctan2";
-        }
-    } };
+            
+            public double compute(double v)
+            {
+                return Math.toRadians(v);
+            }
+            
+            public String getName()
+            {
+                return "toradians";
+            }
+        });
+        Command[] them = new Command[list.size()];
+        list.copyInto(them);
+        return them;
+    }
 }
