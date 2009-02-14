e57901c046226df914e43dc77faff34190fbbd78
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-02-14 05:56:25 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@131 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/mobile/jmlogo/jmlogo/src/org/opengroove/jw/jmlogo/lang/commands/sets/DataProcessingSet2.java
+++ projects/mobile/jmlogo/jmlogo/src/org/opengroove/jw/jmlogo/lang/commands/sets/DataProcessingSet2.java
@@ -1,11 +1,60 @@
 package org.opengroove.jw.jmlogo.lang.commands.sets;
 
+import org.opengroove.jw.jmlogo.lang.InterpreterContext;
+import org.opengroove.jw.jmlogo.lang.InterpreterException;
+import org.opengroove.jw.jmlogo.lang.ListToken;
+import org.opengroove.jw.jmlogo.lang.NamedCommand;
+import org.opengroove.jw.jmlogo.lang.Token;
+import org.opengroove.jw.jmlogo.lang.WordToken;
+
 public class DataProcessingSet2 extends BaseCommandSet
 {
     public static DataProcessingSet2 set = new DataProcessingSet2();
     
     protected void loadCommands()
     {
-        addCommand(new NamedCommand("first", 1, 1){}, new NamedCommand("last", 1, 1), new NamedCommand(""))
+        addCommand(new NamedCommand("first", 1, 1)
+        {
+            
+            public Token run(InterpreterContext context, Token[] arguments)
+            {
+                if (arguments[0] instanceof WordToken)
+                {
+                    WordToken w = (WordToken) arguments[0];
+                    if (w.getValue().length() < 1)
+                        throw new InterpreterException(
+                            "The word specified is empty, so you "
+                                + "can't run the first command on it");
+                    return new WordToken(w.getValue().substring(0, 1));
+                }
+                else
+                {
+                    ListToken t = (ListToken) arguments[0];
+                    if (t.getMembers().length < 1)
+                        throw new InterpreterException(
+                            "The list specified is empty, so you "
+                                + "can't run the first command on it");
+                    
+                }
+            }
+        });
+        addCommand(new NamedCommand("count", 1, 1)
+        {
+            
+            public Token run(InterpreterContext context, Token[] arguments)
+            {
+                if (arguments[0] instanceof WordToken)
+                {
+                    WordToken t = (WordToken) arguments[0];
+                    return new WordToken(t.getValue().length());
+                }
+                else
+                {
+                    ListToken t = (ListToken) arguments[0];
+                    return new WordToken(t.getMembers().length);
+                }
+            }
+        });
+        
     }
 }
