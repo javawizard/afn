41e59aadfa62838dc6ca53e363c9ebe8bd3a3989
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-02-21 03:36:24 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@182 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/learning/jdtype/src/com/googlecode/jwutils/jdtype/JDType.java
+++ projects/learning/jdtype/src/com/googlecode/jwutils/jdtype/JDType.java
@@ -1,5 +1,8 @@
 package com.googlecode.jwutils.jdtype;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class JDType
 {
     
@@ -8,8 +11,16 @@ public class JDType
      */
     public static void main(String[] args)
     {
-        // TODO Auto-generated method stub
-        
+        JFrame frame = new JFrame("JDType");
+        frame.setSize(600, 470);
+        frame.setLocationRelativeTo(null);
+        JLabel label =
+            new JLabel("<html><div style='align: center; font-size: 24px'>"
+                + "Welcome to JDType.</div><br/>More to come soon!"
+                + " -- javawizard2539");
+        label.setHorizontalAlignment(JLabel.CENTER);
+        frame.getContentPane().add(label);
+        frame.show();
     }
     
 }
