f8e7069f4d7a0e9b61d47636f8f99d89d1f7d86f
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-02-21 06:37:55 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@184 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/learning/jdtype/src/com/googlecode/jwutils/jdtype/JDType.java
+++ projects/learning/jdtype/src/com/googlecode/jwutils/jdtype/JDType.java
@@ -17,9 +17,12 @@ public class JDType
         frame.setSize(600, 470);
         frame.setLocationRelativeTo(null);
         JLabel label =
-            new JLabel("<html><div style='align: center; font-size: 24px'>"
-                + "<b>Welcome to JDType.</b></div><br/>More to come soon!"
-                + " -- javawizard2539<br/>&nbsp;<br/>&nbsp;<br/>&nbsp;<br/>&nbsp;");
+            new JLabel(
+                "<html><table border='0' cellspacing='0' cellpadding='3'>"
+                    + "<tr><td style='font-size: 24px' align='center'><b>"
+                    + "Welcome to JMLogo.</b></td></tr>"
+                    + "<tr><td align='center'>More to come soon!"
+                    + " -- javawizard2539</td></tr><br/>&nbsp;<br/>&nbsp;<br/>&nbsp;<br/>&nbsp;");
         label.setFont(Font.decode(null));
         label.setHorizontalAlignment(JLabel.CENTER);
         frame.getContentPane().add(label);
