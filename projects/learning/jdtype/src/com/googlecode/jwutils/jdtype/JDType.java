09cdb979de32fd222bb2e2a8fd0ffa7c9724e3fe
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-02-21 03:05:06 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@181 9038e2a8-f00e-11dd-89c5-9df526a60542

---
+++ projects/learning/jdtype/src/com/googlecode/jwutils/jdtype/JDType.java
@@ -0,0 +1,15 @@
+package com.googlecode.jwutils.jdtype;
+
+public class JDType
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
