5f315836a12e9c13b01b10015397193d7840fba3
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-04-01 23:15:45 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@242 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/jzbot/src/org/opengroove/jzbot/commands/HelpCommand.java
+++ projects/jzbot/src/org/opengroove/jzbot/commands/HelpCommand.java
@@ -1,6 +1,7 @@
 package org.opengroove.jzbot.commands;
 
 import org.opengroove.jzbot.Command;
+import org.opengroove.jzbot.JZBot;
 
 public class HelpCommand implements Command
 {
@@ -13,7 +14,8 @@ public class HelpCommand implements Command
     public void run(String channel, boolean pm, String sender, String hostname,
         String arguments)
     {
-        
+        JZBot.bot.sendMessage(pm ? sender : channel,
+            "Help doesn't work yet. Pm jcp or javawizard2539 for info on jzbot.");
     }
     
 }
