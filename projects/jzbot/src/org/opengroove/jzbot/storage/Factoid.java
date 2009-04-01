91b61ddb9409af241acf3cd842629e76808ce1b6
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-04-01 06:23:14 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@200 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/jzbot/src/org/opengroove/jzbot/storage/Factoid.java
+++ projects/jzbot/src/org/opengroove/jzbot/storage/Factoid.java
@@ -1,9 +1,23 @@
 package org.opengroove.jzbot.storage;
 
+import net.sf.opengroove.common.proxystorage.Property;
 import net.sf.opengroove.common.proxystorage.ProxyBean;
 
 @ProxyBean
 public interface Factoid
 {
+    @Property
+    public String getName();
     
+    public void setName(String name);
+    
+    @Property
+    public boolean isActive();
+    
+    public void setActive(boolean active);
+    
+    @Property
+    public String getValue();
+    
+    public void setValue(String value);
 }
