7184059eccff0a17455f763080da5122d7338079
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-04-01 23:15:05 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@241 9038e2a8-f00e-11dd-89c5-9df526a60542

---
+++ projects/jzbot/src/org/opengroove/jzbot/commands/HelpCommand.java
@@ -0,0 +1,19 @@
+package org.opengroove.jzbot.commands;
+
+import org.opengroove.jzbot.Command;
+
+public class HelpCommand implements Command
+{
+    
+    public String getName()
+    {
+        return "help";
+    }
+    
+    public void run(String channel, boolean pm, String sender, String hostname,
+        String arguments)
+    {
+        
+    }
+    
+}
---
+++ projects/jzbot/src/org/opengroove/jzbot/commands/ShutdownCommand.java
@@ -0,0 +1,20 @@
+package org.opengroove.jzbot.commands;
+
+import org.opengroove.jzbot.Command;
+
+public class ShutdownCommand implements Command
+{
+    
+    public String getName()
+    {
+        return "shutdown";
+    }
+    
+    public void run(String channel, boolean pm, String sender, String hostname,
+        String arguments)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
