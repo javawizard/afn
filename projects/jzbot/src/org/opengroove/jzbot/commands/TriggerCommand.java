af490bc4878be0c9aa74e8de05acd4149619759f
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-04-01 23:43:38 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@256 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/jzbot/src/org/opengroove/jzbot/commands/TriggerCommand.java
+++ projects/jzbot/src/org/opengroove/jzbot/commands/TriggerCommand.java
@@ -21,17 +21,20 @@ public class TriggerCommand implements Command
             return;
         }
         Channel c = JZBot.storage.getChannel(channel);
-        
         if (arguments.equals(""))
         {
-            
+            JZBot.bot.sendMessage(pm ? sender : channel, "The current trigger is "
+                + c.getTrigger());
+            return;
         }
         if (!JZBot.isOp(channel, hostname))
         {
-            JZBot.bot.sendMessage(pm ? sender : channel, "You are not an op here");
+            JZBot.bot.sendMessage(pm ? sender : channel,
+                "You are not an op here, so you can't change the trigger");
             return;
         }
-        
+        c.setTrigger(arguments);
+        JZBot.bot.sendMessage(pm ? sender : channel, "Trigger set to " + arguments);
     }
     
 }
