87c2055a986fe85ad6e0e04e6d35a9fe974a5e19
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-04-01 06:26:34 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@201 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/jzbot/src/org/opengroove/jzbot/storage/Storage.java
+++ projects/jzbot/src/org/opengroove/jzbot/storage/Storage.java
@@ -3,6 +3,7 @@ package org.opengroove.jzbot.storage;
 import net.sf.opengroove.common.proxystorage.ListType;
 import net.sf.opengroove.common.proxystorage.Property;
 import net.sf.opengroove.common.proxystorage.ProxyBean;
+import net.sf.opengroove.common.proxystorage.Search;
 import net.sf.opengroove.common.proxystorage.StoredList;
 
 @ProxyBean
@@ -11,4 +12,14 @@ public interface Storage
     @Property
     @ListType(Channel.class)
     public StoredList<Channel> getChannels();
+    
+    @Search(listProperty = "channels", searchProperty = "name")
+    public Channel getChannel(String name);
+    
+    @Property
+    @ListType(Factoid.class)
+    public StoredList<Factoid> getFactoids();
+    
+    @Search(listProperty = "factoids", searchProperty = "name", exact = false, anywhere = false)
+    public Factoid getFactoid(String name);
 }
