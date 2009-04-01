dc1cbc1092e164bf502c3e423fcacbb6c61ce022
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-04-01 06:10:17 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@196 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/jzbot/src/org/opengroove/jzbot/JZBot.java
+++ projects/jzbot/src/org/opengroove/jzbot/JZBot.java
@@ -6,8 +6,10 @@ import org.jibble.pircbot.IrcException;
 import org.jibble.pircbot.NickAlreadyInUseException;
 import org.jibble.pircbot.PircBot;
 
+/**
+ * jzbot authenticates off of hostmask.
+ */
 public class JZBot extends PircBot
 {
     // numeric 320: is signed on as account
-    
 }
