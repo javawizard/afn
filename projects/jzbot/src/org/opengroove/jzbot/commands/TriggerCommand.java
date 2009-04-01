ea28a6b96d07326f37fd88fdaac7e4f021216402
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-04-01 23:32:03 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@250 9038e2a8-f00e-11dd-89c5-9df526a60542

---
+++ projects/jzbot/src/org/opengroove/jzbot/commands/TriggerCommand.java
@@ -0,0 +1,20 @@
+package org.opengroove.jzbot.commands;
+
+import org.opengroove.jzbot.Command;
+
+public class TriggerCommand implements Command
+{
+    
+    public String getName()
+    {
+        return "trigger";
+    }
+    
+    public void run(String channel, boolean pm, String sender, String hostname,
+        String arguments)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
