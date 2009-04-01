ee489017af21b351bcbd9d1020c80ab7ad6443c8
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-04-01 06:12:19 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@197 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/jzbot/src/org/opengroove/jzbot/JZBot.java
+++ projects/jzbot/src/org/opengroove/jzbot/JZBot.java
@@ -1,15 +1,32 @@
 package org.opengroove.jzbot;
 
+import java.io.File;
 import java.io.IOException;
+
+import net.sf.opengroove.common.proxystorage.ProxyStorage;
 
 import org.jibble.pircbot.IrcException;
 import org.jibble.pircbot.NickAlreadyInUseException;
 import org.jibble.pircbot.PircBot;
+import org.opengroove.jzbot.storage.Storage;
 
 /**
  * jzbot authenticates off of hostmask.
  */
 public class JZBot extends PircBot
 {
+    public static final JZBot bot = new JZBot();
     // numeric 320: is signed on as account
+    private static ProxyStorage proxyStorage;
+    public static Storage storage;
+    
+    public static void main(String[] args) throws Throwable
+    {
+        bot.start();
+    }
+    
+    private void start() throws Throwable
+    {
+        proxyStorage = new ProxyStorage(Storage.class, new File("storage/db"));
+    }
 }
