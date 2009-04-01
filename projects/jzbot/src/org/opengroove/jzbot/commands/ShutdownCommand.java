4420051a4cbc92703ee120c94fd1d73dd3af35f2
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-04-01 23:21:12 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@245 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/jzbot/src/org/opengroove/jzbot/commands/ShutdownCommand.java
+++ projects/jzbot/src/org/opengroove/jzbot/commands/ShutdownCommand.java
@@ -3,6 +3,7 @@ package org.opengroove.jzbot.commands;
 import org.opengroove.jzbot.Command;
 import org.opengroove.jzbot.JZBot;
 import org.opengroove.jzbot.storage.Channel;
+import org.opengroove.jzbot.storage.Operator;
 
 public class ShutdownCommand implements Command
 {
@@ -24,8 +25,19 @@ public class ShutdownCommand implements Command
         long sleepDuration = 5000;
         for (Channel c : JZBot.storage.getChannels().isolate())
         {
-            
+            sleepDuration += 1100;
+            JZBot.bot.sendMessage(c.getName(),
+                "JZBot is shutting down at the request of " + sender);
         }
+        try
+        {
+            Thread.sleep(sleepDuration);
+        }
+        catch (InterruptedException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.exit(0);
     }
-    
 }
