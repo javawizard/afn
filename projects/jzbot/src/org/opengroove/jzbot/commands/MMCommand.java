79d19d2a8758f6359a4476aa0d5ac6b0c41403d9
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-04-02 04:24:55 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@309 9038e2a8-f00e-11dd-89c5-9df526a60542

---
+++ projects/jzbot/src/org/opengroove/jzbot/commands/MMCommand.java
@@ -0,0 +1,27 @@
+package org.opengroove.jzbot.commands;
+
+import org.opengroove.jzbot.Command;
+
+/**
+ * A game of mastermind.
+ * 
+ * @author Alexander Boyd
+ * 
+ */
+public class MMCommand implements Command
+{
+    
+    public String getName()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    public void run(String channel, boolean pm, String sender, String hostname,
+        String arguments)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
