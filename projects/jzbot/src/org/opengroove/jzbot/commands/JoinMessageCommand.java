287b0741c3b03e81fc7fd970f39ad6f62e5097ee
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-04-02 04:24:24 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@308 9038e2a8-f00e-11dd-89c5-9df526a60542

---
+++ projects/jzbot/src/org/opengroove/jzbot/commands/JoinMessageCommand.java
@@ -0,0 +1,21 @@
+package org.opengroove.jzbot.commands;
+
+import org.opengroove.jzbot.Command;
+
+public class JoinMessageCommand implements Command
+{
+    
+    public String getName()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    public void run(String channel, boolean pm, String sender, String hostname,
+        String arguments)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
