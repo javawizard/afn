c8e4f4b37e3670e3473327ca77aac42aee5130f6
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-04-02 04:27:05 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@310 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/jzbot/src/org/opengroove/jzbot/commands/MMCommand.java
+++ projects/jzbot/src/org/opengroove/jzbot/commands/MMCommand.java
@@ -3,7 +3,12 @@ package org.opengroove.jzbot.commands;
 import org.opengroove.jzbot.Command;
 
 /**
- * A game of mastermind.
+ * A game of mastermind. Uses numbers 1 through 5 as "bead colors". 4 beads by
+ * default, I'll probably change that later so you can choose.
+ * 
+ * ~mm reset resets the game. ~mm 2435 guesses that position, which will result
+ * in something like "1 right place, 2 right number wrong place", or
+ * "you win! 2435 was the answer."
  * 
  * @author Alexander Boyd
  * 
