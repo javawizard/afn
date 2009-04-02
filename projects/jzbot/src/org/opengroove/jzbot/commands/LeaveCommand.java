e7200d73984839fb8e81d3287d65e872af533df2
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-04-02 18:05:51 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@382 9038e2a8-f00e-11dd-89c5-9df526a60542

---
+++ projects/jzbot/src/org/opengroove/jzbot/commands/LeaveCommand.java
@@ -0,0 +1,28 @@
+package org.opengroove.jzbot.commands;
+
+import org.opengroove.jzbot.Command;
+
+/**
+ * Leave instructs the bot to depart from a channel and not come back until a
+ * superop requests that it come back. It can be run by superops only.
+ * 
+ * @author Alexander Boyd
+ * 
+ */
+public class LeaveCommand implements Command
+{
+    
+    public String getName()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    public void run(String channel, boolean pm, String sender, String hostname,
+        String arguments)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
