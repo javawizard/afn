6ef2ebef79bd6eef460ee30ea8466b983f46c535
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-04-02 03:00:38 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@291 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/jzbot/src/org/opengroove/jzbot/commands/FactoidCommand.java
+++ projects/jzbot/src/org/opengroove/jzbot/commands/FactoidCommand.java
@@ -1,6 +1,7 @@
 package org.opengroove.jzbot.commands;
 
 import org.opengroove.jzbot.Command;
+import org.opengroove.jzbot.JZBot;
 
 public class FactoidCommand implements Command
 {
@@ -13,7 +14,20 @@ public class FactoidCommand implements Command
     public void run(String channel, boolean pm, String sender, String hostname,
         String arguments)
     {
-        
+        boolean isGlobal = false;
+        if (arguments.startsWith("global "))
+        {
+            isGlobal = true;
+            arguments = arguments.substring("global ".length());
+        }
+        String[] argumentsTokenized1 = arguments.split(" ", 2);
+        String command = argumentsTokenized1[0];
+        if ((!isGlobal) && (channel == null) && (!command.equalsIgnoreCase("isglobal")))
+        {
+            JZBot.bot.sendMessage(pm ? sender : channel,
+                "For non-global commands, you must specify "
+                    + "a channel (unless it is the isglobal command)");
+            return;
+        }
     }
-    
 }
