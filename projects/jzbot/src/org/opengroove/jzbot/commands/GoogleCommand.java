c25b3cbd813cb6613f9ad0fe121ba5132948f3c6
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-04-03 21:10:31 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@400 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/jzbot/src/org/opengroove/jzbot/commands/GoogleCommand.java
+++ projects/jzbot/src/org/opengroove/jzbot/commands/GoogleCommand.java
@@ -1,5 +1,8 @@
 package org.opengroove.jzbot.commands;
 
+import java.net.URL;
+import java.net.URLEncoder;
+
 import org.opengroove.jzbot.Command;
 import org.opengroove.jzbot.ResponseException;
 
@@ -18,7 +21,10 @@ public class GoogleCommand implements Command
             throw new ResponseException("specify search terms after the google command");
         try
         {
-            
+            URL url =
+                new URL(
+                    "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=opengroove"
+                        + URLEncoder.encode(arguments));
         }
         catch (Exception e)
         {
