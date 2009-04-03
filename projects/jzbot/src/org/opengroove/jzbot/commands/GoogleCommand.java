63116a33bc315b1451f8a1c7e894f59584724f52
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-04-03 21:14:18 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@401 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/jzbot/src/org/opengroove/jzbot/commands/GoogleCommand.java
+++ projects/jzbot/src/org/opengroove/jzbot/commands/GoogleCommand.java
@@ -1,8 +1,13 @@
 package org.opengroove.jzbot.commands;
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLEncoder;
 
+import net.sf.opengroove.common.utils.StringUtils;
+
+import org.json.JSONObject;
 import org.opengroove.jzbot.Command;
 import org.opengroove.jzbot.ResponseException;
 
@@ -25,6 +30,12 @@ public class GoogleCommand implements Command
                 new URL(
                     "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=opengroove"
                         + URLEncoder.encode(arguments));
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            InputStream in = url.openStream();
+            StringUtils.copy(in, baos);
+            in.close();
+            JSONObject j = new JSONObject(new String(baos.toByteArray()));
+            sendMessage(pm?sender:channel, j.keys())
         }
         catch (Exception e)
         {
