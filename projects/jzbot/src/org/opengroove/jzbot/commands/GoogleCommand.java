e4c8f41c796891325697e598fd549b3ab65f2878
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-04-03 20:45:24 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@399 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/jzbot/src/org/opengroove/jzbot/commands/GoogleCommand.java
+++ projects/jzbot/src/org/opengroove/jzbot/commands/GoogleCommand.java
@@ -1,21 +1,30 @@
 package org.opengroove.jzbot.commands;
 
 import org.opengroove.jzbot.Command;
+import org.opengroove.jzbot.ResponseException;
 
 public class GoogleCommand implements Command
 {
     
     public String getName()
     {
-        // TODO Auto-generated method stub
-        return null;
+        return "google";
     }
     
     public void run(String channel, boolean pm, String sender, String hostname,
         String arguments)
     {
-        // TODO Auto-generated method stub
-        
+        if (arguments.equals(""))
+            throw new ResponseException("specify search terms after the google command");
+        try
+        {
+            
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e.getClass().getName() + ": " + e.getMessage(),
+                e);
+        }
     }
     
 }
