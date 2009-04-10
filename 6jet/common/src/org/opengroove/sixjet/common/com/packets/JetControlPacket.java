39eb63a274ea7764411edab374a95acf7e293a5f
javawizard@trivergia.com <javawizard@trivergia.com>
2009-04-10 00:53:38 +0000

    Copied from svn://localhost/trivergia/alex/6jet/, rev. 474 by javawizard @ 4/8/2009 8:24 AM
    --HG--
    extra : convert_revision : svn%3A887b5650-2564-11de-af28-f79be8f396a5/trunk%4050

---
+++ 6jet/common/src/org/opengroove/sixjet/common/com/packets/JetControlPacket.java
@@ -0,0 +1,9 @@
+package org.opengroove.sixjet.common.com.packets;
+
+import org.opengroove.sixjet.common.com.Packet;
+
+public class JetControlPacket extends Packet
+{
+    private int jet;
+    private boolean state;
+}
