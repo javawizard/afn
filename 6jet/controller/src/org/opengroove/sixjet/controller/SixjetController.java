6f426a4ba0378fed392f862bd0ed94e523cd699a
javawizard@trivergia.com <javawizard@trivergia.com>
2009-04-10 09:00:43 +0000

    --HG-- extra : convert_revision : svn%3A887b5650-2564-11de-af28-f79be8f396a5/trunk%40170

--- 6jet/controller/src/org/opengroove/sixjet/controller/SixjetController.java
+++ 6jet/controller/src/org/opengroove/sixjet/controller/SixjetController.java
@@ -1,17 +1,45 @@
 package org.opengroove.sixjet.controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import org.opengroove.sixjet.common.ui.LoginFrame;
 import org.opengroove.sixjet.controller.ui.frames.MainFrame;
 
 public class SixjetController
 {
-    private MainFrame mainFrame;
+    public static MainFrame mainFrame;
+    
+    public static LoginFrame loginFrame;
+    
+    public static Socket socket;
+    
+    public static ObjectInputStream inStream;
+    
+    public static ObjectOutputStream outStream;
     
     /**
      * @param args
      */
     public static void main(String[] args)
     {
-        
+        loginFrame = new LoginFrame();
+        loginFrame.getLoginButton().addActionListener(new ActionListener()
+        {
+            
+            public void actionPerformed(ActionEvent e)
+            {
+                attemptLogin();
+            }
+        });
+        loginFrame.setDefaultCloseOperation(loginFrame.EXIT_ON_CLOSE);
+    }
+    
+    protected static void attemptLogin()
+    {
     }
     
 }
