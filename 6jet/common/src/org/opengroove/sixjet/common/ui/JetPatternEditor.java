02b9c9a681a3d67253fb3f91b69010bb192e4e0c
javawizard@trivergia.com <javawizard@trivergia.com>
2009-04-10 01:03:56 +0000

    Copied from svn://localhost/trivergia/alex/6jet/, rev. 511 by javawizard @ 4/9/2009 3:45 AM
    --HG--
    extra : convert_revision : svn%3A887b5650-2564-11de-af28-f79be8f396a5/trunk%4087

---
+++ 6jet/common/src/org/opengroove/sixjet/common/ui/JetPatternEditor.java
@@ -0,0 +1,16 @@
+package org.opengroove.sixjet.common.ui;
+
+import javax.swing.JComponent;
+
+/**
+ * A component that shows a list of tracks, with marks in each track. The
+ * component is created with a specified length, in milliseconds, and it sets
+ * its initial zoom to be 10 milliseconds per pixel.
+ * 
+ * @author Alexander Boyd
+ * 
+ */
+public class JetPatternEditor extends JComponent
+{
+    
+}
