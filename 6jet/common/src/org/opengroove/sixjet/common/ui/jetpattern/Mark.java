95262603361324d18abe0f4831b19e29c9a4223e
javawizard@trivergia.com <javawizard@trivergia.com>
2009-04-10 01:05:26 +0000

    Copied from svn://localhost/trivergia/alex/6jet/, rev. 516 by javawizard @ 4/9/2009 4:38 AM
    --HG--
    extra : convert_revision : svn%3A887b5650-2564-11de-af28-f79be8f396a5/trunk%4092

--- 6jet/common/src/org/opengroove/sixjet/common/ui/jetpattern/Mark.java
+++ 6jet/common/src/org/opengroove/sixjet/common/ui/jetpattern/Mark.java
@@ -1,7 +1,11 @@
 package org.opengroove.sixjet.common.ui.jetpattern;
 
 import javax.swing.JComponent;
-
+/**
+ * A mark on a jet pattern editor. 
+ * @author Alexander Boyd
+ *
+ */
 public class Mark extends JComponent
 {
     
---
+++ 6jet/controller/src/tests/Test02.java
@@ -0,0 +1,15 @@
+package tests;
+
+public class Test02
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
