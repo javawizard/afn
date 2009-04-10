978b9f17699eba73dd8c69125e9a598d22566ef0
javawizard@trivergia.com <javawizard@trivergia.com>
2009-04-10 01:04:49 +0000

    Copied from svn://localhost/trivergia/alex/6jet/, rev. 514 by javawizard @ 4/9/2009 4:35 AM
    --HG--
    extra : convert_revision : svn%3A887b5650-2564-11de-af28-f79be8f396a5/trunk%4090

--- 6jet/common/src/org/opengroove/sixjet/common/ui/JetPatternEditor.java
+++ 6jet/common/src/org/opengroove/sixjet/common/ui/JetPatternEditor.java
@@ -7,9 +7,13 @@ import javax.swing.JComponent;
  * component is created with a specified length, in milliseconds, and it sets
  * its initial zoom to be 10 milliseconds per pixel.
  * 
- * The editor does not automcatically include a scroll pane itself, although it
- * can be asked to create one (which it will then add itself to) by calling
- * createScrollPane.
+ * The editor itself shouldn't be added to a component. Instead, the scroll pane
+ * returned from getScrollPane should be used instead. This scroll pane contains
+ * headers for the jet names, and a time ruler. It will also draw a
+ * "current position" on the ruler, if asked to.
+ * 
+ * The editor draws each channel as a row. Within a channel, marks are present.
+ * Marks are added by clicking and dragging in an area where there is no mark. 
  * 
  * 
  * @author Alexander Boyd
