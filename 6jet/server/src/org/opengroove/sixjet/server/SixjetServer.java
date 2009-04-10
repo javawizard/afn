d529f8514d29d3375bd6bae66cbcc5d2ea513041
javawizard@trivergia.com <javawizard@trivergia.com>
2009-04-10 00:54:59 +0000

    Copied from svn://localhost/trivergia/alex/6jet/, rev. 479 by javawizard @ 4/8/2009 11:59 PM
    --HG--
    extra : convert_revision : svn%3A887b5650-2564-11de-af28-f79be8f396a5/trunk%4055

---
+++ 6jet/server/src/org/opengroove/sixjet/server/SixjetServer.java
@@ -0,0 +1,15 @@
+package org.opengroove.sixjet.server;
+
+public class SixjetServer
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
