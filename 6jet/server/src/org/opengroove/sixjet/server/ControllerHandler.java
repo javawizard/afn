0de7dcb54dc3b3f3717f579511c7525ffd7a6a99
javawizard@trivergia.com <javawizard@trivergia.com>
2009-04-10 21:21:40 +0000

    --HG-- extra : convert_revision : svn%3A887b5650-2564-11de-af28-f79be8f396a5/trunk%40187

--- 6jet/server/src/org/opengroove/sixjet/server/ControllerHandler.java
+++ 6jet/server/src/org/opengroove/sixjet/server/ControllerHandler.java
@@ -10,6 +10,8 @@ import org.opengroove.sixjet.common.com.PacketSpooler;
 
 public class ControllerHandler extends Thread
 {
+    private static final int QUEUE_SIZE = 200;
+    
     private ObjectInputStream in;
     
     private ObjectOutputStream out;
@@ -32,7 +34,9 @@ public class ControllerHandler extends Thread
     
     public void send(Packet packet)
     {
-        spooler.send(packet);
+        if (!spooler.send(packet))
+            throw new RuntimeException("Spooler is full; couldn't send packet to "
+                + username);
     }
     
     public ControllerHandler(Socket socket) throws IOException
@@ -40,6 +44,7 @@ public class ControllerHandler extends Thread
         out = new ObjectOutputStream(socket.getOutputStream());
         out.flush();
         in = new ObjectInputStream(socket.getInputStream());
+        spooler = new PacketSpooler(out, QUEUE_SIZE);
     }
     
     public void run()
