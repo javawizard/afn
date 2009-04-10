e5a7c19cef7e4dd847ecd2d53d65f3d4ff003bc1
javawizard@trivergia.com <javawizard@trivergia.com>
2009-04-10 01:10:37 +0000

    Copied from svn://localhost/trivergia/alex/6jet/, rev. 533 by javawizard @ 4/9/2009 7:47 AM
    --HG--
    extra : convert_revision : svn%3A887b5650-2564-11de-af28-f79be8f396a5/trunk%40109

--- 6jet/controller/src/tests/Test02.java
+++ 6jet/controller/src/tests/Test02.java
@@ -2,6 +2,8 @@ package tests;
 
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JTable;
 
 public class Test02
 {
@@ -17,6 +19,16 @@ public class Test02
         f.setSize(500, 400);
         f.setLocationRelativeTo(null);
         f.show();
+        JInternalFrame if1 = new JInternalFrame();
+        if1.setResizable(true);
+        if1.setMaximizable(false);
+        if1.setIconifiable(false);
+        if1.setClosable(false);
+        if1.setTitle("");
+        if1.setSize(100, 20);
+        d.add(if1);
+        if1.show();
+        JTable t;
     }
     
 }
