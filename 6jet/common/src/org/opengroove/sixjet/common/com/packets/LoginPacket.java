ec94198eedb21cecebce9ba9f10b156285179dce
javawizard@trivergia.com <javawizard@trivergia.com>
2009-04-10 00:53:02 +0000

    Copied from svn://localhost/trivergia/alex/6jet/, rev. 472 by javawizard @ 4/8/2009 8:15 AM
    --HG--
    extra : convert_revision : svn%3A887b5650-2564-11de-af28-f79be8f396a5/trunk%4048

---
+++ 6jet/common/src/org/opengroove/sixjet/common/com/packets/LoginPacket.java
@@ -0,0 +1,9 @@
+package org.opengroove.sixjet.common.com.packets;
+
+import org.opengroove.sixjet.common.com.Packet;
+
+public class LoginPacket extends Packet
+{
+    private String username;
+    private String password;
+}
