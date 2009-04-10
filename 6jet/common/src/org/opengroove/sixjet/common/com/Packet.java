bc52475068fcdc25841514a9b6ce29308063c97b
javawizard@trivergia.com <javawizard@trivergia.com>
2009-04-10 00:51:30 +0000

    Copied from svn://localhost/trivergia/alex/6jet/, rev. 466 by javawizard @ 4/8/2009 8:06 AM
    --HG--
    extra : convert_revision : svn%3A887b5650-2564-11de-af28-f79be8f396a5/trunk%4042

---
+++ 6jet/common/src/org/opengroove/sixjet/common/com/Packet.java
@@ -0,0 +1,15 @@
+package org.opengroove.sixjet.common.com;
+
+import java.io.Serializable;
+
+/**
+ * A 6jet communication packet. Communication in 6jet is done by sending
+ * subclasses of this class via object streams over tcp/ip.
+ * 
+ * @author Alexander Boyd
+ * 
+ */
+public class Packet implements Serializable
+{
+    
+}
