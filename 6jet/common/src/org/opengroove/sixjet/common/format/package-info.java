4fe8a77632eff8c80ae41fccc99300850517eb1d
javawizard@trivergia.com <javawizard@trivergia.com>
2009-04-12 21:59:31 +0000

    --HG-- extra : convert_revision : svn%3A887b5650-2564-11de-af28-f79be8f396a5/trunk%40227

---
+++ 6jet/common/src/org/opengroove/sixjet/common/format/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package's subpackages hold parsers and generators related to each of 6jet's file formats. All 6jet file extensions start with ".6j", so the package name is the letter immediately following 
+ */
+package org.opengroove.sixjet.common.format;
