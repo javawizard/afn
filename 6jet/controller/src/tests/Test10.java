daf2b4455a6a1a3006bf1cce58d69f8d3308dade
javawizard@trivergia.com <javawizard@trivergia.com>
2009-04-15 08:53:30 +0000

    --HG-- extra : convert_revision : svn%3A887b5650-2564-11de-af28-f79be8f396a5/trunk%40313

---
+++ 6jet/controller/src/tests/Test10.java
@@ -0,0 +1,17 @@
+package tests;
+
+public class Test10
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("started");
+        for (int i = 0; i < 1000000000; i++)
+            ;
+        System.out.println("done");
+    }
+    
+}
