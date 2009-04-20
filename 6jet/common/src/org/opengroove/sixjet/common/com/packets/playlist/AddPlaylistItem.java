ba96d24393aeda2c7b2fcfc0414c0f2ae271a6dd
javawizard@trivergia.com <javawizard@trivergia.com>
2009-04-20 04:08:29 +0000

    --HG-- extra : convert_revision : svn%3A887b5650-2564-11de-af28-f79be8f396a5/trunk%40366

--- 6jet/common/src/org/opengroove/sixjet/common/com/packets/playlist/AddPlaylistItem.java
+++ 6jet/common/src/org/opengroove/sixjet/common/com/packets/playlist/AddPlaylistItem.java
@@ -5,13 +5,34 @@ import org.opengroove.sixjet.common.format.l.PlaylistFile.PlaylistItem;
 
 /**
  * Sent from both the controller and the server. When sent from the controller,
- * the item in question includes only 
+ * the item in question includes only the type and the music or delay, not the
+ * id. When sent from the server, all fields are populated.
  * 
  * @author Alexander Boyd
  * 
  */
 public class AddPlaylistItem extends Packet
 {
+    public String getPlaylist()
+    {
+        return playlist;
+    }
+    
+    public PlaylistItem getItem()
+    {
+        return item;
+    }
+    
+    public void setPlaylist(String playlist)
+    {
+        this.playlist = playlist;
+    }
+    
+    public void setItem(PlaylistItem item)
+    {
+        this.item = item;
+    }
+    
     private String playlist;
     private PlaylistItem item;
 }
--- 6jet/common/src/org/opengroove/sixjet/common/com/packets/playlist/RenamePlaylist.java
+++ 6jet/common/src/org/opengroove/sixjet/common/com/packets/playlist/RenamePlaylist.java
@@ -2,7 +2,15 @@ package org.opengroove.sixjet.common.com.packets.playlist;
 
 import org.opengroove.sixjet.common.com.Packet;
 
+/**
+ * Command sent by both the client and the server. It indicates that a playlist
+ * is to be renamed, or that a playlist has been renamed.
+ * 
+ * @author Alexander Boyd
+ * 
+ */
 public class RenamePlaylist extends Packet
 {
-    
+    private String oldName;
+    private String newName;
 }
