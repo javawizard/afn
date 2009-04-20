191ab4c1cab07a434849b7ca44ade98873ac83ed
javawizard@trivergia.com <javawizard@trivergia.com>
2009-04-20 04:02:23 +0000

    --HG-- extra : convert_revision : svn%3A887b5650-2564-11de-af28-f79be8f396a5/trunk%40364

--- 6jet/common/src/org/opengroove/sixjet/common/format/l/PlaylistFile.java
+++ 6jet/common/src/org/opengroove/sixjet/common/format/l/PlaylistFile.java
@@ -2,6 +2,8 @@ package org.opengroove.sixjet.common.format.l;
 
 import java.io.InputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class PlaylistFile implements Serializable
 {
@@ -19,10 +21,81 @@ public class PlaylistFile implements Serializable
         private static final long serialVersionUID = 6431796801693869648L;
         private String id;
         private String type;
+        private int duration;
+        private String music;
+        
+        public String getId()
+        {
+            return id;
+        }
+        
+        public String getType()
+        {
+            return type;
+        }
+        
+        public int getDuration()
+        {
+            return duration;
+        }
+        
+        public String getMusic()
+        {
+            return music;
+        }
+        
+        public void setId(String id)
+        {
+            this.id = id;
+        }
+        
+        public void setType(String type)
+        {
+            this.type = type;
+        }
+        
+        public void setDuration(int duration)
+        {
+            this.duration = duration;
+        }
+        
+        public void setMusic(String music)
+        {
+            this.music = music;
+        }
     }
+    
+    private ArrayList<PlaylistItem> items = new ArrayList<PlaylistItem>();
     
     public PlaylistFile(InputStream in)
     {
-        
+        Scanner scanner = new Scanner(in);
+        scanner.useDelimiter("\n");
+        while (scanner.hasNext())
+        {
+            String token = scanner.next();
+            token = token.trim();
+            if (token.equals(""))
+                continue;
+            Scanner line = new Scanner(token);
+            line.useDelimiter("\\|");
+            PlaylistItem item = new PlaylistItem();
+            String lineId = line.next();
+            item.setId(lineId);
+            String type = line.next();
+            item.setType(type);
+            if (type.equals("music"))
+            {
+                item.setMusic(line.next());
+            }
+            else if (type.equals("delay"))
+            {
+                item.setDuration(line.nextInt());
+            }
+            else
+                throw new RuntimeException("Line type " + type
+                    + " is not music or delay");
+            items.add(item);
+        }
     }
 }
