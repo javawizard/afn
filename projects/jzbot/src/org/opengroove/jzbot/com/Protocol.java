e6507a3ca7366f1017d9e94818cf943951fdfbdb
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-04-26 19:06:17 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@433 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/jzbot/src/org/opengroove/jzbot/com/Protocol.java
+++ projects/jzbot/src/org/opengroove/jzbot/com/Protocol.java
@@ -45,9 +45,9 @@ package org.opengroove.jzbot.com;
  * it wants to allow jzbot to create a factoid for, then it should provide these
  * via the getExtendedEvents method. Then, when one of these events occurs, it
  * should call JZBot.runExtendedEvent. This will run the factoid by the event's
- * name, if one exists, and notify any plugins For example, bzflag defines an
- * extended event called "ontk", which is triggered when a user on the server
- * kills a teammate.
+ * name, if one exists, and notify any plugins about the extended event. For
+ * example, bzflag defines an extended event called "ontk", which is triggered
+ * when a user on the server kills a teammate.
  * 
  * @author Alexander Boyd
  * 
---
+++ projects/jzbot/src/org/opengroove/jzbot/com/bzflag/BZFlagProtocol.java
@@ -0,0 +1,17 @@
+package org.opengroove.jzbot.com.bzflag;
+
+import org.opengroove.jzbot.com.Protocol;
+
+/**
+ * A class that implements the BZFS0026 protocol, used by BZFlag versions 2.x.
+ * When BZFlag 3.0 comes out, I'll probably write a protocol for it.<br/><br/>
+ * 
+ * 
+ * 
+ * @author Alexander Boyd
+ * 
+ */
+public class BZFlagProtocol implements Protocol
+{
+    
+}
