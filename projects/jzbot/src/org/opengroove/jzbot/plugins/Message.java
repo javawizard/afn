83c3dabb85b65c38697dab3952eeea1acc98379f
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-05-04 03:21:19 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@441 9038e2a8-f00e-11dd-89c5-9df526a60542

---
+++ projects/jzbot/src/org/opengroove/jzbot/plugins/Message.java
@@ -0,0 +1,13 @@
+package org.opengroove.jzbot.plugins;
+
+/**
+ * Represents an outbound message.
+ * 
+ * @author Alexander Boyd
+ * 
+ */
+public class Message
+{
+    public boolean action;
+    public String message;
+}
