8e86e5f80d8ac2a49b8fcfc29e1cbd3982eb6068
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-09-30 20:29:37 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@598 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/learning/flashcards/src/test/Test02.java
+++ projects/learning/flashcards/src/test/Test02.java
@@ -1,10 +1,20 @@
 package test;
 
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.util.Arrays;
 
+import javax.imageio.ImageIO;
+import javax.print.DocFlavor;
 import javax.print.DocPrintJob;
 import javax.print.PrintService;
 import javax.print.PrintServiceLookup;
+import javax.print.SimpleDoc;
+import javax.print.attribute.DocAttributeSet;
+import javax.print.attribute.HashDocAttributeSet;
 import javax.print.attribute.HashPrintRequestAttributeSet;
 import javax.print.attribute.Size2DSyntax;
 import javax.print.attribute.standard.MediaPrintableArea;
@@ -27,6 +37,28 @@ public class Test02
         MediaSize mediaSize = MediaSize
                 .getMediaSizeForName(MediaSizeName.NA_LETTER);
         float pageWidth = mediaSize.getX(INCHES);
-        System.out.println(pageWidth);
+        float targetWidth = 3;
+        float pageMiddle = pageWidth / 2;
+        float targetMiddle = targetWidth / 2;
+        attributes.add(new MediaPrintableArea(pageMiddle - targetMiddle, 0, 3,
+                5, INCHES));
+        DocAttributeSet docAttributes = new HashDocAttributeSet();
+        SimpleDoc doc = new SimpleDoc(createData(), DocFlavor.BYTE_ARRAY.PNG,
+                docAttributes);
+        job.print(doc, attributes);
+    }
+    
+    private static Object createData() throws Exception
+    {
+        BufferedImage image = new BufferedImage(300, 500,
+                BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = image.createGraphics();
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, 300, 500);
+        g.setPaint(new GradientPaint(0, 0, Color.BLACK, 0, 500, Color.WHITE));
+        g.fillRect(0, 0, 300, 500);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ImageIO.write(image, "PNG", out);
+        return out.toByteArray();
     }
 }
