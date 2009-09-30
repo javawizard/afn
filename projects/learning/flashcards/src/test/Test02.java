4cf3729f05c7778626c09582659778a084806677
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-09-30 20:08:05 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@595 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/learning/flashcards/src/test/Test02.java
+++ projects/learning/flashcards/src/test/Test02.java
@@ -7,6 +7,7 @@ import javax.print.attribute.HashPrintRequestAttributeSet;
 import javax.print.attribute.Size2DSyntax;
 import javax.print.attribute.standard.MediaPrintableArea;
 import javax.print.attribute.standard.MediaSize;
+import javax.print.attribute.standard.MediaSizeName;
 
 public class Test02
 {
@@ -19,14 +20,15 @@ public class Test02
         PrintService service = PrintServiceLookup.lookupDefaultPrintService();
         DocPrintJob job = service.createPrintJob();
         HashPrintRequestAttributeSet attributes = new HashPrintRequestAttributeSet();
-        attributes.add(new MediaSize(3, 5, Size2DSyntax.INCH));
-        MediaPrintableArea area = (MediaPrintableArea) service
-                .getSupportedAttributeValues(MediaPrintableArea.class, null,
-                        attributes);
-        System.out.println(area.getX(Size2DSyntax.INCH) + " "
-                + area.getY(Size2DSyntax.INCH) + " "
-                + area.getWidth(Size2DSyntax.INCH) + " "
-                + area.getHeight(Size2DSyntax.INCH));
+        attributes.add(MediaSizeName.ISO_B7);
+         MediaPrintableArea area = (MediaPrintableArea) service
+         .getSupportedAttributeValues(MediaPrintableArea.class, null,
+         attributes);
+         System.out.println(area.getX(Size2DSyntax.INCH) + " "
+         + area.getY(Size2DSyntax.INCH) + " "
+         + area.getWidth(Size2DSyntax.INCH) + " "
+         + area.getHeight(Size2DSyntax.INCH));
         // job.print(doc, attributes);
+        System.out.println(MediaSize.findMedia(3, 5, Size2DSyntax.INCH));
     }
 }
