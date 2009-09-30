828804df0df976c38cd516bacdebff917685622b
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-09-30 18:42:55 +0000

    git-svn-id: https://jwutils.googlecode.com/svn/trunk@592 9038e2a8-f00e-11dd-89c5-9df526a60542

---
+++ projects/learning/flashcards/src/test/Test01.java
@@ -0,0 +1,49 @@
+package test;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.print.PageFormat;
+import java.awt.print.Paper;
+import java.awt.print.Printable;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
+
+public class Test01
+{
+    public static class TestPrintable implements Printable
+    {
+        
+        @Override
+        public int print(Graphics graphics, PageFormat format, int pageIndex)
+                throws PrinterException
+        {
+            if (pageIndex != 0)
+                return NO_SUCH_PAGE;
+            Graphics2D g = (Graphics2D) graphics;
+            g.setFont(Font.decode(null).deriveFont(100f));
+            g.setColor(Color.WHITE);
+            g.fillRect((int) format.getImageableX(), (int) format
+                    .getImageableY(), (int) format.getImageableWidth(),
+                    (int) format.getImageableHeight());
+            return PAGE_EXISTS;
+        }
+    }
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Throwable
+    {
+        PrinterJob job = PrinterJob.getPrinterJob();
+        PageFormat format = new PageFormat();
+        format.setOrientation(format.PORTRAIT);
+        Paper paper = new Paper();
+        paper.setSize(3 * 72, 5 * 72);
+        paper.setImageableArea(0, 0, 1200 * 3, 1200 * 5);
+        job.setPrintable(new TestPrintable(), format);
+        job.print();
+    }
+    
+}
