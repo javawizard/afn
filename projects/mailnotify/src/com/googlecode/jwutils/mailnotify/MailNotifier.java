a22ec8a358603bab57217c14650fdf92ba3233d2
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-10-29 00:43:57 +0000

    Added the storage folder to svn:ignore
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@725 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/mailnotify/src/com/googlecode/jwutils/mailnotify/MailNotifier.java
+++ projects/mailnotify/src/com/googlecode/jwutils/mailnotify/MailNotifier.java
@@ -2,41 +2,47 @@ package com.googlecode.jwutils.mailnotify;
 
 import java.util.Properties;
 
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Store;
+
 public class MailNotifier
 {
     
     /**
      * @param args
      */
-    public static void main(String[] args)
+    public static void main(String[] args) throws Throwable
     {
-        String host ="";
+        String host = "imap.gmail.com";
         String username = "";
         String password = "";
-
+        
         // Create empty properties
         Properties props = new Properties();
-
+        
         // Get session
         Session session = Session.getDefaultInstance(props, null);
-
+        
         // Get the store
-        Store store = session.getStore("pop3");
-        store.connect(host, username, password);
-
+        Store store = session.getStore("imap");
+        store.connect(host, 993, username, password);
+        
         // Get folder
         Folder folder = store.getFolder("INBOX");
         folder.open(Folder.READ_ONLY);
-
+        
         // Get directory
         Message message[] = folder.getMessages();
-
-        for (int i=0, n=message.length; i<n; i++) {
-           System.out.println(i + ": " + message[i].getFrom()[0] 
-             + "\t" + message[i].getSubject());
+        
+        for (int i = 0, n = message.length; i < n; i++)
+        {
+            System.out.println(i + ": " + message[i].getFrom()[0] + "\t"
+                    + message[i].getSubject());
         }
-
-        // Close connection 
+        
+        // Close connection
         folder.close(false);
         store.close();
     }
