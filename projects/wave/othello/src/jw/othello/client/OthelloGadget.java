fd8ca23a4e043a62419fb80aeaa390ed13f4ec84
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-11-05 07:53:47 +0000

    Experimenting with a HelloWorld gadget
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@762 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/wave/othello/src/jw/othello/client/OthelloGadget.java
+++ projects/wave/othello/src/jw/othello/client/OthelloGadget.java
@@ -2,14 +2,18 @@ package jw.othello.client;
 
 import com.google.gwt.gadgets.client.Gadget;
 import com.google.gwt.gadgets.client.UserPreferences;
+import com.google.gwt.gadgets.client.Gadget.ModulePrefs;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
 
+@ModulePrefs(author = "Alexander Boyd", height = 100, title = "Othello")
 public class OthelloGadget extends Gadget<UserPreferences>
 {
-
+    
     @Override
     protected void init(UserPreferences preferences)
     {
-        
+        RootPanel.get().add(new Label("Hello world!!!!"));
     }
     
 }
