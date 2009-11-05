1f286c726cadae44e67a6433a55fd8f39a4ddb3a
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-11-05 08:10:03 +0000

    Now adding a test button
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@769 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/wave/othello/src/jw/othello/client/OthelloGadget.java
+++ projects/wave/othello/src/jw/othello/client/OthelloGadget.java
@@ -3,17 +3,32 @@ package jw.othello.client;
 import com.google.gwt.gadgets.client.Gadget;
 import com.google.gwt.gadgets.client.UserPreferences;
 import com.google.gwt.gadgets.client.Gadget.ModulePrefs;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 @ModulePrefs(author = "Alexander Boyd", height = 100, title = "Othello")
+@SuppressWarnings("deprecation")
 public class OthelloGadget extends Gadget<UserPreferences>
 {
     
     @Override
     protected void init(UserPreferences preferences)
     {
-        RootPanel.get().add(new Label("Hello world!!!!"));
+        Button button = new Button("Say hi");
+        button.addClickListener(new ClickListener()
+        {
+            
+            @Override
+            public void onClick(Widget sender)
+            {
+                Window.alert("Hello world");
+            }
+        });
+        RootPanel.get().add(button);
     }
     
 }
