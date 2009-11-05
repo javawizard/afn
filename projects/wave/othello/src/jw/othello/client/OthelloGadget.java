7b0d050c2ce01bdb75619490917798ab2a8735db
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-11-05 08:26:16 +0000

    Testing out some wave features
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@771 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/wave/othello/src/jw/othello/client/OthelloGadget.java
+++ projects/wave/othello/src/jw/othello/client/OthelloGadget.java
@@ -1,6 +1,10 @@
 package jw.othello.client;
 
 import org.cobogw.gwt.waveapi.gadget.client.NeedsWave;
+import org.cobogw.gwt.waveapi.gadget.client.ParticipantUpdateEvent;
+import org.cobogw.gwt.waveapi.gadget.client.ParticipantUpdateEventHandler;
+import org.cobogw.gwt.waveapi.gadget.client.StateUpdateEvent;
+import org.cobogw.gwt.waveapi.gadget.client.StateUpdateEventHandler;
 import org.cobogw.gwt.waveapi.gadget.client.WaveFeature;
 
 import com.google.gwt.gadgets.client.Gadget;
@@ -15,31 +19,40 @@ import com.google.gwt.user.client.ui.Widget;
 
 @ModulePrefs(author = "Alexander Boyd", height = 100, title = "Othello")
 @SuppressWarnings("deprecation")
-public class OthelloGadget extends Gadget<UserPreferences> implements NeedsWave
+public class OthelloGadget extends Gadget<UserPreferences> implements NeedsWave,
+        StateUpdateEventHandler, ParticipantUpdateEventHandler
 {
+    public static OthelloGadget singleton;
     
     private WaveFeature wave;
+    
+    public Label initialLoadingLabel = new Label("Loading Othello...");
     
     @Override
     protected void init(UserPreferences preferences)
     {
-        Button button = new Button("Say hi");
-        button.addClickListener(new ClickListener()
-        {
-            
-            @Override
-            public void onClick(Widget sender)
-            {
-                Window.alert("Hello world");
-            }
-        });
-        RootPanel.get().add(button);
+        singleton = this;
+        RootPanel.get().add(initialLoadingLabel);
+        wave.addParticipantUpdateEventHandler(this);
+        wave.addStateUpdateEventHandler(this);
     }
     
     @Override
     public void initializeFeature(WaveFeature feature)
     {
         this.wave = feature;
+    }
+    
+    @Override
+    public void onUpdate(StateUpdateEvent event)
+    {
+        RootPanel.get().add(new Label("State updated"));
+    }
+    
+    @Override
+    public void onUpdate(ParticipantUpdateEvent event)
+    {
+        RootPanel.get().add(new Label("Participants updated"));
     }
     
 }
