78c8119f0cd86f5921cba0828c2d0103e5ad6f7f
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-11-05 07:39:39 +0000

    Working on some more initial code
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@760 9038e2a8-f00e-11dd-89c5-9df526a60542

---
+++ projects/wave/othello/src/jw/othello/client/OthelloGadget.java
@@ -0,0 +1,15 @@
+package jw.othello.client;
+
+import com.google.gwt.gadgets.client.Gadget;
+import com.google.gwt.gadgets.client.UserPreferences;
+
+public class OthelloGadget extends Gadget<UserPreferences>
+{
+
+    @Override
+    protected void init(UserPreferences preferences)
+    {
+        
+    }
+    
+}
