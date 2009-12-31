159a53ed630fa995c4d16ad317bb2ce72a9163fa
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-12-31 08:14:04 +0000

    Added .* and classes to svn:ignore, and checked in the initial interpreter. It can't do much yet, but it's starting to take shape.
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@991 9038e2a8-f00e-11dd-89c5-9df526a60542

---
+++ projects/esolangs/gibberish/src/Gibberish.java
@@ -0,0 +1,39 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.math.MathContext;
+import java.util.Deque;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class Gibberish
+{
+    public static Deque<Object> stack = new LinkedBlockingDeque<Object>();
+    
+    public static MathContext mc = new MathContext(300);
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws IOException
+    {
+        if (args.length == 0)
+        {
+            System.out.println("Usage: gibberish <filename> -- Runs the specified file, "
+                    + "as gibberish code. Input will be provided via stdin; "
+                    + "output will be sent to stdout.");
+        }
+        FileInputStream in = new FileInputStream(args[0]);
+        String code = new Scanner(in).useDelimiter("\\z").next();
+        run(code);
+    }
+    
+    private static void run(String code)
+    {
+        int index = 0;
+        while (index < code.length())
+        {
+            index += 1;
+        }
+    }
+}
