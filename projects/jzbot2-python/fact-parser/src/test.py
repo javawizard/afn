54166a602ad4a60a928469430225b5412fe9fa90
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2010-01-05 08:23:07 +0000

    Example demonstrating use of functions
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@1014 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/jzbot2-python/fact-parser/src/test.py
+++ projects/jzbot2-python/fact-parser/src/test.py
@@ -2,7 +2,7 @@ from factparser.parser import parse
 from factparser.parser import FactContext
 from factparser.sinks import PrintSink
 
-text = "I'd like to print %test%.";
+text = "I'd like to print %test%. {lset|the|Hello there}and %the% again.";
 context = FactContext()
 context.localVars['test'] = "This is some text";
 factoid = parse(text); 
