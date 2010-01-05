8e67f6349b1e2775eda2684392507d6348cccec2
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2010-01-05 04:48:50 +0000

    More work on this thing
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@1010 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/jzbot2-python/fact-parser/src/factparser/entities.py
+++ projects/jzbot2-python/fact-parser/src/factparser/entities.py
@@ -14,3 +14,37 @@ class VarReference:
         
     def resolve(self, sink, context):
         sink.write(context.localVars[self.name])
+        
+class FunctionReference:
+    def __init__(self, arguments):
+        self.arguments = arguments
+        
+class Sequence:
+    def __init__(self):
+        self.list = []
+        
+    def length(self):
+        return len(self.list)
+    
+    def get(self, index):
+        return self.list[index];
+    
+    def add(self, item):
+        self.list.append(item);
+        
+    def remove(self, index):
+        del self.list[index]
+        
+    def resolve(self, sink, context):
+        for item in self.list:
+            item.resolve(sink, context)
+        
+
+
+
+
+
+
+
+
+
--- projects/jzbot2-python/fact-parser/src/factparser/parser.py
+++ projects/jzbot2-python/fact-parser/src/factparser/parser.py
@@ -1,13 +1,55 @@
+from charstack import *
 from entities import *
 from sinks import *
 
-def parse():
-    pass
-
+def parse(text):
+    stack = CharStack("{identity|" + text + "}")
+    entity = parseFunction(stack)
+    # TODO: we need to do some checking here to make sure they 
+    # didn't include a premature function close or supply an
+    # out-of-function "|" character
+    return entity
+    
+def parseFunction(stack):
+    if(stack.next() != "{"):
+        raise ParseException("Start of function reference must be an open brace but is not")
+    argumentSequence = Sequence()
+    currentArgument = Sequence()
+    argumentSequence.add(currentArgument)
+    currentLiteral = None
+    while stack.more():
+        c = stack.next();
+        if c == "\n" or c == "\r":
+            continue
+        elif c == "\\":
+            if(currentLiteral == None):
+                currentLiteral = Literal()
+    
 class FactContext:
     globalVars = {}
     def __init__(self):
         self.localVars = {}
         self.subroutines = {}
         self.action = False
+
+class ParseException(Exception):
+    def __init__(self, reason):
+        self.reason = reason;
+        
+    def __str__(self):
+        return "Syntax exception while parsing factoid: " + self.reason
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
