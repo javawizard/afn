9cb7eb1da8179a13b170951e1c3be08d2e0e01bc
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2010-01-05 09:04:41 +0000

    More examples. That's it for tonight.
    git-svn-id: https://jwutils.googlecode.com/svn/trunk@1018 9038e2a8-f00e-11dd-89c5-9df526a60542

--- projects/jzbot2-python/fact-parser/src/test.py
+++ projects/jzbot2-python/fact-parser/src/test.py
@@ -1,6 +1,6 @@
 from factparser.parser import parse
 from factparser.parser import FactContext
-from factparser.sinks import PrintSink
+from factparser.sinks import PrintSink, StringSink
 
 # Note that the factoid language as interpreted by marlen has
 # changed since the move from marlen_jackson to multimarlen
@@ -15,6 +15,7 @@ from factparser.sinks import PrintSink
 
 # Anyway, here are some examples showing how to use the interpreter.
 # Note the usage of PrintSink(); this constructs a new PrintSink
+
 # which is a type of Sink that simply writes output using Python's
 # print function. Later on in the file is a demonstration of how
 # to use a StringSink to actually get the factoid's output. Note,
@@ -23,9 +24,9 @@ from factparser.sinks import PrintSink
 # "print" statement after each time we run a factoid.
 
 # Demonstrate choosing a random item
-text = "The {random|first|second|third} number was randomly chosen.";
+text = "The {random|first|second|third} number was randomly chosen."
 context = FactContext()
-factoid = parse(text); 
+factoid = parse(text) 
 factoid.resolve(PrintSink(), context)
 print ""
 
@@ -33,7 +34,7 @@ print ""
 text = "The var is %myvar% and I'm about to set a var, too. {lset|another|BYEEEEE THAR}"
 context = FactContext()
 context.localVars['myvar'] = "HELLOOOOOOO world!"
-factoid = parse(text); 
+factoid = parse(text) 
 factoid.resolve(PrintSink(), context)
 print ""
 print "After factoid invocation, a variable called \"another\"'s value is " + \
@@ -43,11 +44,34 @@ print "After factoid invocation, a variable called \"another\"'s value is " + \
 # a space tells the factoid interpreter to act as if it weren't there. Note we're
 # double-backslashing stuff since if it were only one backslash, python would
 # interpret the backslash itself.
-text = "This is the first line.\\nSecond line. And there's no\\ space here.";
+text = "This is the first line.\\nSecond line. And there's no\\ space here."
 context = FactContext()
-factoid = parse(text); 
+factoid = parse(text) 
 factoid.resolve(PrintSink(), context)
 print ""
+
+# Demostrate the ifeq function.
+text = "{ifeq|first|second|Yep.|Nope.} {ifeq|third|third|Yep.|Nope.}"
+context = FactContext()
+factoid = parse(text) 
+factoid.resolve(PrintSink(), context)
+print ""
+
+# Demostrate the ifneq function, which is the opposite of {ifeq}
+text = "{ifneq|first|second|Yep.|Nope.} {ifneq|third|third|Yep.|Nope.}"
+context = FactContext()
+factoid = parse(text) 
+factoid.resolve(PrintSink(), context)
+print ""
+
+# Demonstrate writing to a StringSink instead of a PrintSink
+text = "Hello, I'm some factoid output. {ifeq|hah|hah|Yep.|Nope.}"
+context = FactContext()
+factoid = parse(text)
+sink = StringSink()
+factoid.resolve(sink, context)
+result = sink.toString()
+print 'The factoid\'s output was "' + result + '".'
 
 
 
