702f1d3a0c62831a897f3e6420deaef16db9d018
jcp <jcp@3d13355d-ed97-4537-bd08-250cb908a2e5>
2010-10-09 23:39:22 +0000

    Documented the synchronized decorator
    git-svn-id: svn://spiro.afn/afn@19 3d13355d-ed97-4537-bd08-250cb908a2e5

--- afn/python/src/concurrent.py
+++ afn/python/src/concurrent.py
@@ -3,6 +3,21 @@ from threading import RLock
 from functools import partial, update_wrapper
 
 def synchronized(lock=None, function=None):
+    """
+    Returns a function wrapping the specified function that locks on the
+    specified lock before calling the function and releases it after the 
+    function has been invoked. Omitting the second argument will return
+    functools.partial(synchronized, lock), and specifying a callable lock will
+    instead return synchronized(None, lock), which effectively allows this
+    function to be used as a decorator thus:
+    
+    @synchronized(some_lock)
+    @synchronized()
+    @synchronized
+    
+    All three uses are valid, with the latter two creating a new reentrant
+    lock for this function.
+    """
     if callable(lock): # in case it's used as @synchronized
         return synchronized(None, lock)
     if function is None:
