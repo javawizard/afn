959680138d59a1103eac1036d81449a44c2bab66
jcp <jcp@3d13355d-ed97-4537-bd08-250cb908a2e5>
2010-10-09 18:01:04 +0000

    Fixed autobus1 so that it can be killed with ctrl+c
    git-svn-id: svn://spiro.afn/afn@16 3d13355d-ed97-4537-bd08-250cb908a2e5

--- afn/python/src/afntest/autobus1.py
+++ afn/python/src/afntest/autobus1.py
@@ -1,5 +1,6 @@
 
 from libautobus import AutobusConnection
+from time import sleep
 
 class ExampleInterface(object):
     """
@@ -19,3 +20,11 @@ server = AutobusConnection()
 server.add_interface("example", ExampleInterface(), None)
 server.connect()
 
+
+# That's it for the actual autobus code. The rest of the code is a hack to get
+# around python not letting the application die with ctrl+c.
+try:
+    while True:
+        sleep(1)
+except KeyboardInterrupt:
+    server.shutdown()
