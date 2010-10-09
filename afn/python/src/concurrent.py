20aa50eae48c20ba79762a434c307d0851ceee3e
jcp <jcp@3d13355d-ed97-4537-bd08-250cb908a2e5>
2010-10-09 23:32:30 +0000

    Added a synchronized function/decorator
    git-svn-id: svn://spiro.afn/afn@18 3d13355d-ed97-4537-bd08-250cb908a2e5

--- afn/python/src/concurrent.py
+++ afn/python/src/concurrent.py
@@ -1,4 +1,20 @@
 
+from threading import RLock
+from functools import partial, update_wrapper
+
+def synchronized(lock=None, function=None):
+    if callable(lock): # in case it's used as @synchronized
+        return synchronized(None, lock)
+    if function is None:
+        return partial(synchronized, lock)
+    if lock is None:
+        lock = RLock()
+    def wrapper(*args, **kwargs):
+        with lock:
+            function(*args, **kwargs)
+    update_wrapper(wrapper, function)
+    return wrapper
+
 class AtomicInteger(object):
     """
     An integer or long (it switches between the two as needed) that provides
