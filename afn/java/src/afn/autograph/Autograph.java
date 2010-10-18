ad4bf8e4831af3d74ddc918071716f9858fa4ffe
jcp <jcp@3d13355d-ed97-4537-bd08-250cb908a2e5>
2010-10-18 21:50:22 +0000

    git-svn-id: svn://spiro.afn/afn@55 3d13355d-ed97-4537-bd08-250cb908a2e5

---
+++ afn/java/src/afn/autograph/Autograph.java
@@ -0,0 +1,15 @@
+package afn.autograph;
+
+public class Autograph
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
