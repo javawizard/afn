fbf4181d51bd4eb0709980ccc6af60b63c514005
jcp <jcp@3d13355d-ed97-4537-bd08-250cb908a2e5>
2010-11-02 17:57:49 +0000

    Added the timernotify script, a whole bunch of technotes, and created a blank script for hvacd (the future heating and air-conditioning controller daemon)
    git-svn-id: svn://spiro.afn/afn@74 3d13355d-ed97-4537-bd08-250cb908a2e5

---
+++ afn/python/src/timernotify.py
@@ -0,0 +1,70 @@
+
+import sys
+from libautobus import AutobusConnection
+from optparse import OptionParser
+from time import sleep
+
+def main():
+    try:
+        import pynotify
+    except ImportError:
+        print "You don't have the Python libnotify binding installed."
+        sys.exit()
+    
+    if not pynotify.init("timernotify"):
+        print "Libnotify could not be initialized."
+        sys.exit()
+    
+    parser = OptionParser(usage="usage: python -m timernotify [options]", 
+            add_help_option=False)
+    parser.add_option("-h", type="string", action="store", dest="host", default="localhost",
+            help="The host that the Autobus server is running on. Without this "
+            "option, localhost will be used.")
+    parser.add_option("-s", action="store_const", const=True, dest="state_change",
+            default=False, help="If present, all state changes (instead of just "
+            "when the timer beeps) will cause a notification to be shown.")
+    parser.add_option("-?", "--help", action="help")
+    
+    options, command_line_args = parser.parse_args()
+    
+    bus = AutobusConnection(host=options.host)
+    def beeping_listener(timer):
+        pynotify.Notification("Timer " + str(timer), "is beeping.").show()
+    
+    def state_change_listener(timer, state):
+        if state == 1:
+            state_string = "counting up"
+        if state == 2:
+            state_string = "counting down"
+        if state == 3:
+            state_string = "stopped"
+        pynotify.Notification("Timer " + str(timer), "is now " + state_string + 
+                ".").show()
+    
+    bus.add_event_listener("timer", "beeping", beeping_listener)
+    if options.state_change:
+        bus.add_event_listener("timer", "manual_state_change", state_change_listener)
+    
+    bus.connect()
+    
+    try:
+        while True:
+            sleep(1)
+    except KeyboardInterrupt:
+        print "Interrupted, shutting down"
+    finally:
+        bus.shutdown()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
