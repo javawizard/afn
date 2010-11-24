c13bba88aec56825236779006a097824bd780002
jcp <jcp@3d13355d-ed97-4537-bd08-250cb908a2e5>
2010-11-24 10:02:20 +0000

    libstatmonitor works prefectly, and monitord does now too. They only show CPU usage at the moment; no memory, but that'll come soon enough.
    git-svn-id: svn://spiro.afn/afn@106 3d13355d-ed97-4537-bd08-250cb908a2e5

--- afn/python/src/monitord.py
+++ afn/python/src/monitord.py
@@ -3,21 +3,25 @@ from libautobus import AutobusConnection
 import sys
 from time import sleep
 import re
+from libstatmonitor import CPUMonitor
 
 def main():
     global status_object
+    global cpu_monitor
     if len(sys.argv) <= 1:
         print "You need to specify a name for this monitor instance."
         sys.exit()
     bus = AutobusConnection()
     bus.add_interface("monitor." + sys.argv[1])
-    status_object = bus.add_object("monitor." + sys.argv[1], "status", 
+    status_object = bus.add_object("monitor." + sys.argv[1], "status",
             "Provides system status information such as current CPU "
             "usage, memory usage, etc, about this machine.", None)
     bus.start_connecting()
+    cpu_monitor = CPUMonitor()
+    cpu_monitor.refresh()
     try:
         while True:
-            sleep(3)
+            sleep(5)
             update_status()
     except KeyboardInterrupt:
         print "Interrupted, shutting down"
@@ -30,15 +34,10 @@ cpu_user_nice = {}
 cpu_system = {}
 
 def update_status():
-    current_cpu_user = {}
-    current_cpu_system = {}
-    cpu_list = []
-    with open("/proc/stat") as proc_stat:
-        for line in proc_stat:
-            if re.match("cpu[0-9] .*", line):
-                this_user, this_system = re.match(
-                        "cpu. +([0-9]+) ([0-9]+)")
-
+    cpu_monitor.refresh()
+    cpu_list = [cpu_monitor.get_condensed(cpu) for cpu 
+            in range(cpu_monitor.get_processors())]
+    status_object.set({"cpu": cpu_list})
 
 
 
