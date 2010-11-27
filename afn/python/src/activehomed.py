ca0b2c5b0f5735df3b2f74735f2f375fbed42c53
jcp <jcp@3d13355d-ed97-4537-bd08-250cb908a2e5>
2010-11-27 23:40:08 +0000

    git-svn-id: svn://spiro.afn/afn@120 3d13355d-ed97-4537-bd08-250cb908a2e5

--- afn/python/src/activehomed.py
+++ afn/python/src/activehomed.py
@@ -2,6 +2,7 @@
 from libautobus import AutobusConnection
 from traceback import print_exc
 import sys
+import os
 try:
     from win32com.client import Dispatch #@UnresolvedImport
 except ImportError:
@@ -10,8 +11,16 @@ except ImportError:
     print "need to have pywin32 installed."
     sys.exit()
 
+class RPC(object):
+    def action(self, mode, *args):
+        args = " ".join(str(arg) for arg in args)
+        print "Mode: " + str(mode) + ", command: " + str(args)
+        activehome.SendAction(mode, args)
+        return "successful"
+
 def main():
     global activehome
+    global bus
     try:
         activehome = Dispatch("X10.ActiveHome")
     except:
@@ -19,3 +28,29 @@ def main():
         print "You need to install the ActiveHome Scripting SDK before you"
         print "can use activehomed."
         sys.exit()
+    print "Connecting to server " + os.getenv("AUTOBUS_SERVER")
+    bus = AutobusConnection()
+    bus.add_interface("activehome", RPC())
+    bus.start_connecting()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
