1c084b107e6c107dbed167353debd8e50400eeaa
jcp <jcp@3d13355d-ed97-4537-bd08-250cb908a2e5>
2010-12-13 21:28:20 +0000

    git-svn-id: svn://spiro.afn/afn@186 3d13355d-ed97-4537-bd08-250cb908a2e5

---
+++ afn/python/src/tkthread.py
@@ -0,0 +1,70 @@
+
+from threading import Thread
+from Queue import Queue, Empty
+from traceback import print_exc
+
+class TkThread(object):
+    """
+    This class starts a thread that runs Tkinter.mainloop(). It allows UI
+    tasks to be submitted to it, which it will execute as fast as it can and
+    in order. The return value from those tasks can be captured if needed. This
+    makes it a bit easier for multithreaded applications to use Tkinter.
+    """
+    def __init__(self, init_function):
+        """
+        Creates a new TkThread. init_function is a function that, when called,
+        should create the root Tk window and return it.
+        
+        After you create a new TkThread, you need to call its start function
+        to actually get things going.
+        """
+        self.init_function = init_function
+        self.queue = Queue()
+    
+    def run(self):
+        toplevel = self.init_function()
+        toplevel.after(100, self.process_events)
+        toplevel.mainloop()
+    
+    def process_events(self):
+        try:
+            while True:
+                event = self.queue.get_nowait()
+                try:
+                    event()
+                except:
+                    print_exc()
+        except Empty:
+            pass
+    
+    def call(self, function, *args):
+        """
+        Schedules the specified function to be called on the Tkinter thread.
+        """
+        self.queue.put(lambda: function(*args))
+    
+    def call_sync(self, function, *args):
+        """
+        Schedules the specified function to be called on the Tkinter thread,
+        then waits until it gets called and returns its return value.
+        """
+        wait_queue = Queue()
+        def event():
+            try:
+                value = (False, function(*args))
+            except Exception, e:
+                value = (True, e)
+            wait_queue.put(value)
+        self.call(event)
+        excepted, value = wait_queue.get()
+        if excepted:
+            raise value
+        else:
+            return value
+
+
+
+
+
+
+
