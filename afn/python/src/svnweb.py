e635bfef408a337fe6d8e4f8d6a43a669a7a3f95
jcp <jcp@3d13355d-ed97-4537-bd08-250cb908a2e5>
2010-12-27 23:20:35 +0000

    This should be the last spacing change
    git-svn-id: svn://spiro.afn/afn@227 3d13355d-ed97-4537-bd08-250cb908a2e5

--- afn/python/src/svnweb.py
+++ afn/python/src/svnweb.py
@@ -193,7 +193,7 @@ def display_mediawiki(request, path, text):
     .content div[class~="mwx.paragraph"] {margin-bottom: 11px}
     
     .content > div > h1 {font-size: 24px; width: 100%%;
-                         border-bottom: 1px solid #aaa; margin-bottom: 13px;
+                         border-bottom: 1px solid #aaa; margin-bottom: 12px;
                          margin-top: 8px}
     
     .content > div > div > h2 {font-size: 19px; width: 100%%; 
