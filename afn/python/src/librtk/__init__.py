f677868fd6289a40da13c14e8a86e84f58f43b3b
jcp <jcp@3d13355d-ed97-4537-bd08-250cb908a2e5>
2011-01-02 06:55:02 +0000

    Started work
    git-svn-id: svn://spiro.afn/afn@255 3d13355d-ed97-4537-bd08-250cb908a2e5

--- afn/python/src/librtk/__init__.py
+++ afn/python/src/librtk/__init__.py
@@ -2,6 +2,7 @@
 from threading import Thread, RLock
 from Queue import Queue
 from traceback import print_exc
+import default_widget_schema
 
 class EventThread(Thread):
     """
@@ -31,11 +32,110 @@ class EventThread(Thread):
             except:
                 print_exc()
 
+class Event(object):
+    """
+    An event manager. Listeners can be added by calling the listen function.
+    Listeners can be removed by calling the unlisten function. The replace
+    function removes all listeners and then adds one specific listener, in
+    essence replacing all of the current listeners with the one specified.
+    
+    Calling an instance of this class fires the event. All arguments passed
+    to the invocation will be passed to each listener. If any of the
+    listeners raise an exception, the exception's traceback will be printed
+    to stdout, but the exception will be otherwise suppressed (and in
+    particular, all remaining listeners will still be called).
+    """
+    def __init__(self):
+        """
+        Creates a new event.
+        """
+        self.listeners = []
+    
+    def listen(self, function):
+        """
+        Adds the specified function as a listener to this event. If the
+        function is already present on this event, it will not be added twice.
+        """
+        if function not in self.listeners:
+            self.listeners.append(function)
+    
+    def unlisten(self, function):
+        """
+        Removes the specified function from this event's list of listeners.
+        """
+        try:
+            self.listeners.remove(function)
+        except ValueError:
+            pass
+    
+    def __call__(self, *args, **kwargs):
+        """
+        Fires all listeners registered to this event. The specified arguments
+        (including keyword arguments) will be passed to all of the listeners.
+        """
+        for listener in self.listeners[:]: # Clone in case someone modifies us
+            try:
+                listener(*args, **kwargs)
+            except:
+                print_exc()
+
 class Connection(object):
-    pass
+    def __init__(self, connect_function, validators=[],
+            use_default_widgets=True):
+        try:
+            self.protocol_init
+        except AttributeError:
+            raise Exception("Connection cannot be instantiated directly. You "
+                    "need to instantiate one of its subclasses. "
+                    "ThreadedServer and AsyncoreServer are two such "
+                    "subclasses included with librtk. Try "
+                    "help(librtk.ThreadedServer) or help(librtk.AsyncoreServer) "
+                    "for more information.")
+        self.connect_function = connect_function
+        self.validators = validators[:]
+        if use_default_widgets:
+            self.validators.append(default_validator)
+        self.children = []
+        self.widgets = {}
+        self.widget_schema = {}
+        self.protocol_init()
+    
+    def start(self):
+        self.protocol_start()
+    
+    def protocol_receive(self, data):
+        """
+        Called by subclasses of Connection when they have new data for
+        Connection to process. data should be the return value of json.loads
+        or a compatible object.
+        """
+    
+    def protocol_connection_lost(self):
+        """
+        Called by subclasses of Connection when the connection has been lost.
+        If Connection previously called protocol_close (which subclasses
+        themselves implement), the connection is not required (but is allowed)
+        to call this function.
+        """
+    
+def default_validator(features, schema):
+    schema.update(default_widget_schema.schema)
 
 class ThreadedServer(Thread):
-    pass
+    def protocol_init(self):
+        pass
+    
+    def protocol_start(self):
+        pass
+    
+    def protocol_send(self, data):
+        pass
+    
+    def protocol_event_ready(self, data):
+        pass
+    
+    def protocol_close(self):
+        pass
 
 class ThreadedConnection(Connection):
     pass
