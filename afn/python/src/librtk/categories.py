e7b7aac1f4f4f866f81327e335f92ddfec553ad8
jcp <jcp@3d13355d-ed97-4537-bd08-250cb908a2e5>
2011-01-02 05:10:05 +0000

    Added schemas for Window, VBox, Label, and Button
    git-svn-id: svn://spiro.afn/afn@252 3d13355d-ed97-4537-bd08-250cb908a2e5

--- afn/python/src/librtk/categories.py
+++ afn/python/src/librtk/categories.py
@@ -2,3 +2,5 @@
 TOPLEVEL = "toplevel"
 CONTAINER = "container"
 WIDGET = "widget"
+
+number = (int, long)
--- afn/python/src/librtk/default_widget_schema.py
+++ afn/python/src/librtk/default_widget_schema.py
@@ -1,18 +1,46 @@
 
-from categories import TOPLEVEL, CONTAINER, WIDGET
+from categories import TOPLEVEL, CONTAINER, WIDGET, number
 
 schema = {
     "Window": [
-        "window", TOPLEVEL, [
-            ["widget_attribute", writable, default_value],
-        ], [
-            ["layout_attribute", writable, default_value],
-        ], [
-            ["state_attribute", default_value],
-        ], [
-            ["call_name", [param1_type, param2_type], return_type],
-        ], [
-            ["event_name"],
+        "Window", TOPLEVEL, [ # Widget attributes:
+            ["title", True, ""],
+        ], [ # Layout attributes:
+        ], [ # State attributes:
+            ["size", [0, 0]],
+        ], [ # Calls:
+            ["pack"],
+            ["resize"],
+            ["relocate"],
+            ["center"],
+        ], [ # Events:
+        ]
+    ],
+    "VBox": [
+        "VBox", CONTAINER, [ # Widget attributes:
+        ], [ # Layout attributes:
+        ], [ # State attributes:
+        ], [ # Calls:
+        ], [ # Events:
+        ]
+    ],
+    "Label": [
+        "Label", WIDGET, [ # Widget attributes:
+            ["text", True, ""],
+        ], [ # Layout attributes:
+        ], [ # State attributes:
+        ], [ # Calls:
+        ], [ # Events:
+        ]
+    ],
+    "Button": [
+        "Button", WIDGET, [ # Widget attributes:
+            ["text", True, ""],
+        ], [ # Layout attributes:
+        ], [ # State attributes:
+        ], [ # Calls:
+        ], [ # Events:
+            ["clicked"],
         ]
     ],
 }
@@ -21,15 +49,15 @@ schema = {
 Template for an individual widget:
 
     "WidgetConstructorName": [
-        "widget_name", CATEGORY, [
+        "widget_name", CATEGORY, [ # Widget attributes:
             ["widget_attribute", writable, default_value],
-        ], [
+        ], [ # Layout attributes:
             ["layout_attribute", writable, default_value],
-        ], [
+        ], [ # State attributes:
             ["state_attribute", default_value],
-        ], [
+        ], [ # Calls:
             ["call_name", [param1_type, param2_type], return_type],
-        ], [
+        ], [ # Events:
             ["event_name"],
         ]
     ],
