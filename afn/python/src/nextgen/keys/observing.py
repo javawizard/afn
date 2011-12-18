9cb9047214b21304e1ac4fb10ded20dc2c103624
Alexander Boyd <alex@opengroove.org>
2011-12-17 22:50:35 -0700

    ObservableDict is written. Now to see if it works...

--- afn/python/src/nextgen/keys/observing.py
+++ afn/python/src/nextgen/keys/observing.py
@@ -1,18 +1,108 @@
 
+from afn.utils import singleton
+
+# _NO_VALUE = singleton.Singleton("nextgen.keys.observing._NO_VALUE")
 
 class Observable(object):
     def _get_current_values(self, value_list):
         pass
     
     def _recursive_listener(self, source, changes):
-        pass
+        for observer, registration in self._observers.items():
+            if registration["recursive"]:
+                observer(self, changes)
     
     def _init_observable(self):
         try:
             self._observers
         except AttributeError:
             self._observers = {}
+            self._recursive_observer_count = 0
     
-    def observe(self, listener, recursive=False):
+    def observe(self, observer, recursive=False):
         self._init_observable()
-        
+        if observer in self._observers:
+            raise ValueError("The specified observer has already been added to "
+                             "this object.")
+        registration = {"recursive": recursive}
+        self._observers[observer] = registration
+        if recursive:
+            self._recursive_observer_count += 1
+            if self._recursive_observer_count == 1: # This was the first
+                # recursive observer to be added
+                values = []
+                self._get_current_values(values)
+                for value in values:
+                    if isinstance(value, Observable):
+                        value.observe(self._recursive_listener, True)
+    
+    def unobserve(self, observer):
+        self._init_observable()
+        if observer not in self._observers:
+            raise ValueError("The specified observer has not been added as to "
+                             "this object.")
+        registration = self._observers[observer]
+        del self._observers[observer]
+        if registration["recursive"]:
+            self._recursive_observer_count -= 1
+            if self._recursive_observer_count == 0: # Last recursive observer
+                # to be removed
+                values = []
+                self._get_current_values(values)
+                for value in values:
+                    if isinstance(value, Observable):
+                        value.unobserve(self._recursive_listener)
+    
+    def _notify_changed(self, changes):
+        if len(changes) == 0:
+            return
+        if self._recursive_observer_count > 0:
+            for change in changes:
+                if hasattr(change, "old"):
+                    if isinstance(change.old, Observable):
+                        change.old.unobserve(self._recursive_listener)
+                if hasattr(change, "new"):
+                    if isinstance(change.new, Observable):
+                        change.new.observe(self._recursive_listener, True)
+        for observer in self._observers:
+            observer(self, changes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ afn/python/src/nextgen/keys/types.py
@@ -0,0 +1,128 @@
+
+from nextgen.keys.observing import Observable
+from nextgen.keys import changes as cc
+from collections import OrderedDict
+from afn.utils.singleton import Singleton as _Singleton
+
+_NONE = _Singleton("nextgen.keys.types._NONE")
+
+class ObservableDict(Observable, dict):
+    def _get_current_values(self, value_list):
+        value_list += self.itervalues()
+        super(ObservableDict, self)._get_current_values(value_list)
+    
+    def __delitem__(self, key):
+        value = self[key]
+        super(ObservableDict, self).__delitem__(key)
+        self._notify_changed([cc.KeyRemoved(self, key, value)])
+    
+    def __setitem__(self, key, value):
+        exists = key in self
+        if exists:
+            old = self[key]
+        self[key] = value
+        if exists:
+            self._notify_changed([cc.KeyUpdated(self, key, old, value)])
+        else:
+            self._notify_changed([cc.KeyAdded(self, key, value)])
+    
+    def clear(self):
+        items = self.items()
+        super(ObservableDict, self).clear()
+        self._notify_changed([cc.KeyRemoved(self, k, v) for (k, v) in items])
+    
+    def copy(self):
+        return ObservableDict(self.iteritems())
+    
+    def pop(self, key, default=_NONE):
+        if key not in self:
+            if default is _NONE:
+                raise KeyError(key)
+            else:
+                return default
+        value = self[key]
+        del self[key] # This will call __delitem__, which will handle posting
+        # the KeyRemoved event
+        return value
+    
+    def popitem(self):
+        if len(self) == 0:
+            raise KeyError()
+        key, value = self.iteritems().next()
+        del self[key] # This will handle posting the KeyRemoved event
+        return key, value
+    
+    def setdefault(self, key, default=None):
+        if key not in self:
+            self[key] = default # This will handle posting the KeyAdded event
+            return default
+        return self[key]
+    
+    def update(self, e, **f):
+        changes = []
+        if hasattr(e, "keys"):
+            for key in e:
+                value = e[key]
+                if key in self:
+                    changes.append(cc.KeyUpdated(self, key, self[key], value))
+                else:
+                    changes.append(cc.KeyAdded(self, key, value))
+                super(ObservableDict, self).__setitem__(key, value)
+        else:
+            for key, value in e:
+                if key in self:
+                    changes.append(cc.KeyUpdated(self, key, self[key], value))
+                else:
+                    changes.append(cc.KeyAdded(self, key, value))
+                super(ObservableDict, self).__setitem__(key, value)
+        for key, value in f.iteritems():
+            if key in self:
+                changes.append(cc.KeyUpdated(self, key, self[key], value))
+            else:
+                changes.append(cc.KeyAdded(self, key, value))
+            super(ObservableDict, self).__setitem__(key, value)
+        self._notify_changed(changes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
