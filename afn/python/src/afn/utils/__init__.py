6f5aaa91ad5311dde1eab9a3e18d5c85d8434ab4
Alexander Boyd <alex@opengroove.org>
2011-12-18 01:06:17 -0700

    Changing slicer not to work with negative indexes for now since the logic behind their workings appears to be somewhat screwy

--- afn/python/src/afn/utils/__init__.py
+++ afn/python/src/afn/utils/__init__.py
@@ -1,28 +1,17 @@
 
 def slicer(length, start=None, stop=None, step=None):
-    if step == 0:
-        raise ValueError("step can't be 0")
+    if step <= 0:
+        raise ValueError("step can't be 0 or negative. Negative steps will be supported soon.")
     if start is None:
-        if step < 0:
-            start = length
-        else:
-            start = 0
+        start = 0
     if stop is None:
-        if step < 0:
-            stop = 0
-        else:
-            stop = length
+        stop = length
     if step is None:
         step = 1
     if start < 0:
         start += length
     if stop < 0:
         stop += length
-    if step > 0:
-        while start < stop:
-            yield start
-            start += step
-    else:
-        while start > stop:
-            yield start
-            start += step
+    while start < stop:
+        yield start
+        start += step
