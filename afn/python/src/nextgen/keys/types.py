e62e69e63b835e847a03c401ff5d79b9afa76c8d
Alexander Boyd <alex@opengroove.org>
2011-12-18 23:53:52 -0700

    List stuffs should be nearly done

--- afn/python/src/nextgen/keys/types.py
+++ afn/python/src/nextgen/keys/types.py
@@ -98,7 +98,76 @@ class ObservableList(Observable, list):
     
     # TODO: finish this up, and use afn.utils.slicer to implement slicing, and
     # see if Py3 has that and submit it as a patch if it doesn't
-
+    
+    def __delitem__(self, s):
+        if isinstance(s, (int, long)):
+            s = slice(s, s + 1)
+        indexes = list(slicer(len(self), s.start, s.stop, s.step))
+        changes = [cc.ItemRemoved(self, i, self[i]) for i in indexes]
+        super(ObservableList, self).__delitem__(s)
+        self._notify_changed(changes)
+    
+    def __delslice__(self, start, stop):
+        self.__delitem__(slice(start, stop))
+    
+    def __iadd__(self, items):
+        index = len(self)
+        super(ObservableList, self).__iadd__(items)
+        changes = [cc.ItemInserted(self, i, self[i]) for i in range(index, len(self))]
+        self._notify_changed(changes)
+    
+    def __imul__(self, number):
+        index = len(self)
+        super(ObservableList, self).__imul__(number)
+        changes = [cc.ItemInserted(self, i, self[i]) for i in range(index, len(self))]
+        self._notify_changed(changes)
+    
+    def __setitem__(self, s, item):
+        if isinstance(s, (int, long)):
+            old = self[s]
+            super(ObservableList, self).__setitem__(s, item)
+            self._notify_changed([cc.ItemUpdated(self, s, old, item)])
+        else:
+            raise NotImplementedError("Assignments to list slices of "
+                                      "ObservableList instances haven't been "
+                                      "implemented yet.")
+    
+    def __setslice__(self, start, stop, item):
+        self.__setitem__(slice(start, stop), item)
+    
+    def append(self, item):
+        super(ObservableList, self).append(item)
+        self._notify_changed([cc.ItemInserted(self, len(self) - 1, item)])
+    
+    def extend(self, iterable):
+        # As far as I can tell, extend and __iadd__ behave the exact same way
+        self += iterable
+    
+    def insert(self, index, item):
+        super(ObservableList, self).insert(index, item)
+        self._notify_changed([cc.ItemInserted(self, index, item)])
+    
+    def pop(self, index=None):
+        if index is None:
+            index = len(self) - 1
+        item = self.pop(index)
+        self._notify_changed([cc.ItemRemoved(self, index, item)])
+    
+    def remove(self, value):
+        del self[self.index(value)] # __del__ will handle posting the
+        # ItemRemoved notification for us
+    
+    def reverse(self):
+        super(ObservableList, self).reverse()
+        length = len(self)
+        highest = length - 1
+        changes = [cc.ItemsSwapped(self, highest - i, i, self[i], self[highest - i]) for i in range(length / 2)]
+        self._notify_changed(changes)
+    
+    def sort(self, *args):
+        raise NotImplementedError("Sorting an OrderedList is not currently "
+                                  "supported. It will be supported at some "
+                                  "point in the near future.")
 
 
 
