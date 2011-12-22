ceb6bd7091e11aebd79f44a9704b479465dceacd
Alexander Boyd <alex@opengroove.org>
2011-12-21 18:32:51 -0700

    Fixed a bug in listeners.print_service_discovered

--- afn/python/src/autobus2/landfill/listeners.py
+++ afn/python/src/autobus2/landfill/listeners.py
@@ -2,4 +2,4 @@
 from autobus2 import DISCOVERED, CHANGED, UNDISCOVERED 
 
 def print_service_listener(service_id, host, port, info, event):
-    print "SERVICE " + str(event).rpartition(".")[2] + ": " + service_id + " on " + host + ":" + port + " -- " + str(info)
+    print "SERVICE " + str(event).rpartition(".")[2] + ": " + service_id + " on " + host + ":" + str(port) + " -- " + str(info)
