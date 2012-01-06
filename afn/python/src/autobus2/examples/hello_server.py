4025a9500d1fd4308ccfe087c6bed7d82c394ea1
Alexander Boyd <alex@opengroove.org>
2012-01-06 16:01:36 -0700

    Changed hello_server to use a class and create_service(use_py_object=...)

--- afn/python/src/autobus2/examples/hello_server.py
+++ afn/python/src/autobus2/examples/hello_server.py
@@ -1,13 +1,14 @@
 
 from autobus2 import Bus, wait_for_interrupt
 
+class HelloService(object):
+    def hi(self, text="world"):
+        print "Saying hi to " + str(text)
+        return "Hi, " + str(text) + "! How are you?"
+
+
 def main():
     with Bus() as bus:
-        service = bus.create_service({"autobus.example": "hello_server"})
-        def hi(text="world"):
-            print "Saying hi to " + str(text)
-            return "Hi, " + str(text) + "! How are you?"
-        service.create_function("hi", hi)
-        service.activate()
+        bus.create_service({"autobus.example": "hello_server"}, from_py_object=HelloService())
         
         wait_for_interrupt()
