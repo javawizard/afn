c2507d5978424a22deb37e67db4aa42976aa5ab9
Alexander Boyd <alex@opengroove.org>
2012-01-06 15:12:31 -0700

    Removed hello_server's reference to print_service_listener. I think I'm going to write an example in a bit that's purpose-designed to print out discovery information, which I can then use instead of hello_server to see if various machines are able to discover services on other machines.

--- afn/python/src/autobus2/examples/hello_server.py
+++ afn/python/src/autobus2/examples/hello_server.py
@@ -1,10 +1,8 @@
 
 from autobus2 import Bus, wait_for_interrupt
-from autobus2.landfill.listeners import print_service_listener
 
 def main():
     with Bus() as bus:
-        bus.add_service_listener(print_service_listener, initial=True)
         service = bus.create_service({"autobus.example": "hello_server"})
         def hi(text="world"):
             print "Saying hi to " + str(text)
