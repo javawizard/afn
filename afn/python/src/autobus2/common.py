2d5dd13776b6a126bcaa7b193108cea42e6ea71c
Alexander Boyd <alex@opengroove.org>
2012-01-06 15:38:46 -0700

    Working on merging the stuff common to connections and service proxies into ServiceConnector

--- afn/python/src/autobus2/common.py
+++ afn/python/src/autobus2/common.py
@@ -1,4 +1,7 @@
 
+from abc import ABCMeta as ABC, abstractmethod as abstract
+import autobus2
+
 class AutoClose(object):
     """
     A class intended to be used as a superclass by classes with a close()
@@ -19,3 +22,48 @@ class AutoClose(object):
         self._autoclose_enters -= 1
         if self._autoclose_enters == 0:
             self.close()
+
+
+class ServiceConnector(object):
+    __metaclass__ = ABC
+    
+    @abstract
+    def _call_function(self, args, callback, timeout, safe):
+        pass
+    
+    def __getitem__(self, name):
+        return self.functions[name]
+    
+    def _sc_get_function(self, name):
+        return ServiceConnectorFunction(self, name)
+    
+    @property
+    def functions(self):
+        # TODO: cache this
+        return ServiceConnectorAccessor(self._sc_get_function)
+
+
+class ServiceConnectorAccessor(object):
+    def __init__(self, accessor):
+        self.accessor = accessor
+    
+    def __getattr__(self, name):
+        return self.accessor(name)
+    
+    def __getitem__(self, name):
+        return self.accessor(name)
+
+
+class ServiceConnectorFunction(object):
+    def __init__(self, connector, name):
+        self.connector = connector
+        self.name = name
+    
+    def __call__(self, *args, **kwargs):
+        callback = kwargs.get("callback", autobus2.SYNC)
+        timeout = kwargs.get("timeout", 30)
+        safe = kwargs.get("safe", False)
+        return self.connector._call_function(args, callback, timeout, safe)
+
+
+
