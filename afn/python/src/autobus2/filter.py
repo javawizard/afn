45003b9474d9c762c318dd8fe5c4409ce1e7234f
Alexander Boyd <alex@opengroove.org>
2012-01-07 13:22:36 -0700

    Added a filtering thingie to filter by things that don't have a particular value

--- afn/python/src/autobus2/filter.py
+++ afn/python/src/autobus2/filter.py
@@ -1,5 +1,6 @@
 
 from afn.utils import singleton
+from collections import namedtuple
 
 ANY = singleton.Singleton("autobus2.filter.ANY")
 NOT_PRESENT = singleton.Singleton("autobus2.filter.NOT_PRESENT")
@@ -14,7 +15,13 @@ def filter_matches(info, filter):
         elif v is NOT_PRESENT:
             if k in info:
                 return False
+        elif isinstance(v, NotEqualTo):
+            if info.get(k) == v.value:
+                return False
         else:
-            if info[k] != v:
+            if info.get(k) != v:
                 return False
     return True
+
+
+NotEqualTo = namedtuple("NotEqualTo", ["value"])
