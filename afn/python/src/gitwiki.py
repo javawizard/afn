ea4c01c430f908ffda32a692530529b22eafe1eb
Alexander Boyd <alex@opengroove.org>
2012-01-09 14:25:11 -0700

    Sidetracked on writing a git-based wiki... I think I'll get back to that in a bit. Oh, and noting, at some point I want to build a little thingie that runs on a machine for a particular git repository and publishes the last revision that's been committed to it as an Autobus object, and other machines can then watch that and automatically fetch updates. It needs more thought but it's something I'm working on.

---
+++ afn/python/src/gitwiki.py
@@ -0,0 +1,9 @@
+
+"""
+This module provides a simple wiki backed by a git repository.
+
+It's a really simple wiki for now. It doesn't even require a password to log
+in; you simply enter your name and email address as you want them to show up in
+the commit log, or leave them blank to use the name and email address
+configured in the repository or on your machine.
+"""
