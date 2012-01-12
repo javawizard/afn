2448a453bfbcc78ad67a72b25012d6e73675644a
Alexander Boyd <alex@opengroove.org>
2012-01-12 16:58:21 -0700

    Object changing appears to work

--- afn/python/src/autobus2/examples/testobjectserver1.py
+++ afn/python/src/autobus2/examples/testobjectserver1.py
@@ -1,9 +1,15 @@
 
 import autobus2
+import time
 
 def main():
     with autobus2.Bus() as bus:
         service = bus.create_service({"autobus.example": "testobjectserver1"})
-        object = service.create_object("test", "hello")
+        object = service.create_object("test", time.time())
         service.activate()
-        autobus2.wait_for_interrupt()
+        while True:
+            try:
+                time.sleep(3)
+                object.set_value(time.time())
+            except KeyboardInterrupt:
+                return
