95fcf5a7c5fb6b1849223d1d4c300abd0a09aaa7
Alexander Boyd <alex@opengroove.org>
2012-01-16 01:46:51 -0700

    Modifications to the schedule tool, don't remember exactly what though...

--- afn/python/src/uofu/schedule.py
+++ afn/python/src/uofu/schedule.py
@@ -3,6 +3,8 @@ from BeautifulSoup import BeautifulSoup
 from urllib import urlopen
 import re
 from collections import namedtuple
+from traceback import print_exc
+import json
 
 
 ClassSeating = namedtuple("ClassSeating", ["id", "department", "number", "section", "title", "max", "current", "available"])
@@ -16,10 +18,10 @@ def read_page(url):
     return urlopen(url).read()
 
 
-def get_website(term=1118):
+def get_website(term=1124):
     """
     Gets the URL for the main campus schedule website using the specified term.
-    The default, 1118, is Fall 2011; this value can be obtained by going to
+    The default, 1124, is Spring 2012; this value can be obtained by going to
     http://www.utah.edu/students/catalog.php, clicking on the appropriate link
     on the right, and examining the term= query parameter in the URL.
     """
@@ -47,7 +49,7 @@ def yield_departments(website=get_website(), progress=True):
         yield name, description
 
 
-def yield_class_seatings(department, term=1118, progress=True):
+def yield_class_seatings(department, term=1124, progress=True):
     url = "http://www.acs.utah.edu/uofu/stu/scheduling/crse-info?term=%s&subj=%s"
     url = url % (term, department)
     if progress:
@@ -60,8 +62,23 @@ def yield_class_seatings(department, term=1118, progress=True):
         tds = tr.findAll("td")
         t = [extract_text(td).strip() for td in tds]
         yield ClassSeating(t[0], t[1], t[2], t[3], t[4], int(t[5]), int(t[6]), int(t[7]))
-        
-        
+
+
+def yield_all_available_class_seatings(term=1124, progress=True):
+    for department_name, department_desc in yield_departments(get_website(term), progress):
+        for seating in yield_class_seatings(department_name, term, progress):
+            try:
+                if seating.available > 0:
+                    yield seating
+            except:
+                print_exc()
+
+
+def write_all_seatings_to_file(filename):
+    with open(filename, "w") as f:
+        for department_name, department_desc in yield_departments():
+            for seating in yield_class_seatings(department_name):
+                f.write(json.dumps(seating._asdict()) + "\n")
     
 
 
