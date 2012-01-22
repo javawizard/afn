6066f7b284e7166116a8e126ac2cda39823e4834
Alexander Boyd <alex@opengroove.org>
2012-01-21 17:28:58 -0700

    Added the doorbell script and modified it to work with Autobus 2. This hasn't been tested yet.

---
+++ afn/python/src/doorbell.py
@@ -0,0 +1,30 @@
+import sys
+from traceback import print_exc
+# sys.path.append("/afn/afn-python/src")
+
+from parallel import Parallel #@UnresolvedImport
+from autobus2 import Bus
+from time import sleep
+
+p = Parallel()
+
+previous = True
+
+with Bus() as bus:
+    with bus.get_service_proxy({"type": "speak"}, multiple=True) as s: 
+        while True:
+            current = p.getInPaperOut()
+            if current != previous:
+                previous = current
+                if not current: # Button was just pressed (i.e. the paperOut pin
+                    # was just shorted to ground)
+                    print "Doorbell was pressed"
+                    try:
+                        s["say_text"]("someone is ringing the_front doorbell", callback=None)
+                    except:
+                        print_exc()
+                    sleep(5)
+            sleep(0.03)
+
+
+
