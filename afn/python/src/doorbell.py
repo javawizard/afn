91f6a170284b770d7df688529a577c8ef5c35439
Alexander Boyd <alex@opengroove.org>
2012-01-21 18:21:28 -0700

    .

--- afn/python/src/doorbell.py
+++ afn/python/src/doorbell.py
@@ -8,27 +8,26 @@ from time import sleep
 from afn.utils import print_args
 from functools import partial
 
-p = Parallel()
-
-previous = True
-
-with Bus() as bus:
-    bus.add_service_listener(partial(print_args, "SERVICE LISTENER"), initial=True)
-    with bus.get_service_proxy({"type": "speak"}, bind_function=partial(print_args, "BIND"),
-            unbind_function=partial(print_args, "UNBIND"), multiple=True) as s: 
-        while True:
-            current = p.getInPaperOut()
-            if current != previous:
-                previous = current
-                if not current: # Button was just pressed (i.e. the paperOut pin
-                    # was just shorted to ground)
-                    print "Doorbell was pressed"
-                    try:
-                        print s["say_text"]("someone is ringing the_front doorbell", callback=partial(print_args, "CALLBACK"))
-                    except:
-                        print_exc()
-                    sleep(5)
-            sleep(0.03)
+def run():
+    p = Parallel()
+    previous = True
+    with Bus() as bus:
+        bus.add_service_listener(partial(print_args, "SERVICE LISTENER"), initial=True)
+        with bus.get_service_proxy({"type": "speak"}, bind_function=partial(print_args, "BIND"),
+                unbind_function=partial(print_args, "UNBIND"), multiple=True) as s: 
+            while True:
+                current = p.getInPaperOut()
+                if current != previous:
+                    previous = current
+                    if not current: # Button was just pressed (i.e. the paperOut pin
+                        # was just shorted to ground)
+                        print "Doorbell was pressed"
+                        try:
+                            print s["say_text"]("someone is ringing the_front doorbell", callback=partial(print_args, "CALLBACK"))
+                        except:
+                            print_exc()
+                        sleep(5)
+                sleep(0.03)
 
 
 
