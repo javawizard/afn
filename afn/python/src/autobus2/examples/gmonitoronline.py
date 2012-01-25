0a701304b13a8c1e944c71970ba2e296db635113
Alexander Boyd <alex@opengroove.org>
2012-01-24 17:29:47 -0700

    Wrote a sample GTK app. GTK's easier than I remember it being...

--- afn/python/src/autobus2/examples/gmonitoronline.py
+++ afn/python/src/autobus2/examples/gmonitoronline.py
@@ -1,1 +1,21 @@
 
+import gtk
+import gtk.gdk
+import gobject
+import time
+from threading import Thread
+from functools import partial
+from autobus2 import Bus
+
+def main():
+    gobject.threads_init()
+    window = gtk.Window()
+    window.set_title("hello world")
+    window.show()
+    def recurring():
+        for i in range(3):
+            time.sleep(1)
+            gobject.idle_add(partial(window.set_title, "number " + str(i)))
+        gobject.idle_add(window.destroy)
+    Thread(target=recurring).start()
+    gtk.main()
