ac3094bd450112bd4903c3efdc7c9ae86014aef4
Alexander Boyd <alex@opengroove.org>
2012-02-09 22:24:13 -0700

    The University Credit Union client library is now able to log in, but their site just went down (I swear I didn't do it...), so I'll have to wait until later to keep developing this.

--- afn/python/src/afn/ucreditu/client.py
+++ afn/python/src/afn/ucreditu/client.py
@@ -2,6 +2,8 @@
 from BeautifulSoup import BeautifulSoup
 import urllib
 import urllib2
+from getpass import getpass
+import re
 
 def main():
     print "Testing out the University Federal Credit Union client..."
@@ -10,7 +12,11 @@ def main():
 class Session(object):
     # Thanks to http://stackoverflow.com/a/4836113/992720 for the general idea
     # of how to track cookies and redirects
-    def __init__(self, username, password, login=True):
+    def __init__(self, username, password=None, login=True):
+        if password is None:
+            password = getpass("Enter your password: ")
+        self.username = username
+        self.password = password
         cookies = urllib2.HTTPCookieProcessor()
         redirector = urllib2.HTTPRedirectHandler()
         self._opener = urllib2.build_opener(redirector, cookies)
@@ -22,3 +28,42 @@ class Session(object):
     
     def _post(self, url, parameters):
         return self._opener.open(url, urllib.urlencode(parameters)).read()
+    
+    def login(self):
+        main_page = BeautifulSoup(self._get("https://mobile.ucreditu.com/"))
+        login_url = main_page.find("a", {"name": "LogIn"})["href"]
+        login_page = BeautifulSoup(self._get("https://mobile.ucreditu.com/" + login_url[1:]))
+        login_submit_url = login_page.find("form", {"name": "signin"})["action"]
+        summary_page = self._post("https://mobile.ucreditu.com/" + login_submit_url[1:],
+                {"userNumber": self.username, "password": self.password, "Login": "Login"})
+        print summary_page
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
