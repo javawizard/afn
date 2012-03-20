cef1313ab9e135b88db714a822e962a6f3291f53
Alexander Boyd <alex@opengroove.org>
2012-03-20 02:06:25 -0600

    Yay, discarding works brilliantly

--- afn/java/src/afn/parcon/examples/Test1.java
+++ afn/java/src/afn/parcon/examples/Test1.java
@@ -5,11 +5,11 @@ import afn.parcon.Parser;
 
 public class Test1 {
     public static void main(String[] args) {
-        Parser parser = significant("a").then(significant("b"))
-                .then(first(significant("c"),
-                        significant("d").then(significant("e"))));
+        Parser parser = significant("a").then(significant("b")).then(
+                first(significant("c"),
+                        significant("d").discard().then(significant("e"))));
         Parser whitespace = literal(" ");
-        String text = "a   b  cdefg";
+        String text = "a   b  defg";
         System.out.println(parser);
         System.out.println(parser.parse(text, 0, text.length(), whitespace));
     }
