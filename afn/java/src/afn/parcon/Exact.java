34413cb5afbf2c177401896c263aba5bc7c5e36c
Alexander Boyd <alex@opengroove.org>
2012-03-20 19:12:44 -0600

    Hm, doesn't quite work...

--- afn/java/src/afn/parcon/Exact.java
+++ afn/java/src/afn/parcon/Exact.java
@@ -4,13 +4,20 @@ public class Exact extends Parser {
     
     private Parser parser;
     
+    private Parser whitespace;
+    
     public Exact(Parser parser) {
+        this(parser, Invalid.invalid);
+    }
+    
+    public Exact(Parser parser, Parser whitespace) {
         this.parser = parser;
+        this.whitespace = whitespace;
     }
     
     @Override
     public Result parse(String text, int position, int end, Parser space) {
-        // TODO Auto-generated method stub
-        return null;
+        position = space.consume(text, position, end);
+        return parser.parse(text, position, end, this.whitespace);
     }
 }
--- afn/java/src/afn/parcon/Optional.java
+++ afn/java/src/afn/parcon/Optional.java
@@ -3,15 +3,24 @@ package afn.parcon;
 public class Optional extends Parser {
     
     private Parser parser;
+    private Object defaultResult;
     
     public Optional(Parser parser) {
+        this(parser, null);
+    }
+    
+    public Optional(Parser parser, Object defaultResult) {
         this.parser = parser;
+        this.defaultResult = defaultResult;
     }
     
     @Override
     public Result parse(String text, int position, int end, Parser space) {
-        // TODO Auto-generated method stub
-        return null;
+        Result result = parser.parse(text, position, end, space);
+        if (result.matched)
+            return result;
+        else
+            return new Result(end, defaultResult, result.expected);
     }
     
 }
