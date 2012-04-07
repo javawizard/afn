0b56b52eb614b7f60035d50718c49853d176ae16
Alexander Boyd <alex@opengroove.org>
2012-04-07 01:43:10 -0600

    Working on adding afn-java as a dependency to jzbot

--- afn/java/src/afn/udt/ArrayCollection.java
+++ afn/java/src/afn/udt/ArrayCollection.java
@@ -1,5 +1,163 @@
 package afn.udt;
 
+import java.util.Iterator;
+
 public class ArrayCollection<I, V> extends AbstractCollection<I, V> {
+    
+    @Override
+    public void add(int index, I item, V value) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    @Override
+    public void set(int index, I item, V value) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    @Override
+    public void put(int index, I item, V value) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    @Override
+    public void remove(int index) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+    @Override
+    public Iterable<Entry<I, V>> entries() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    @Override
+    public I getItem(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    @Override
+    public V get(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    @Override
+    public int getIndex(I item) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+    @Override
+    public V get(I item) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    @Override
+    public int getLastIndex(I item) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+    @Override
+    public V getLast(I item) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    @Override
+    public Collection<Integer, Void> getIndexes(I item) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    @Override
+    public Collection<V, Void> getValues(I item) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    @Override
+    public I getItemForValue(V value) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    @Override
+    public int getIndexForValue(V value) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+    @Override
+    public I getLastItemForValue(V value) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    @Override
+    public int getLastIndexForValue(V value) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+    @Override
+    public Collection<I, Void> getItemsForValue(V value) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    @Override
+    public Collection<Integer, Void> getIndexesForValue(V value) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    @Override
+    public boolean isOrdered() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+    @Override
+    public boolean allowsDuplicates() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+    @Override
+    public boolean acceptsValues() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+    @Override
+    public boolean hasItem(I item) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+    @Override
+    public boolean hasValue(V value) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+    @Override
+    public Iterator<I> iterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
