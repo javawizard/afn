6610bf395689a4d0407b453f4b93148c7dade649
Alexander Boyd <alex@opengroove.org>
2012-07-19 01:48:40 -0600

    Starting work on the event loop

---
+++ afn/python/src/afn/utils/cad/eventloop.py
@@ -0,0 +1,48 @@
+"""
+This module provides an event loop system.
+
+The blist module must be installed for this to work. It can be obtained with
+pip install blist.
+"""
+
+try:
+    from blist import sorteddict
+except ImportError:
+    print "You don't appear to have the blist module installed. You can"
+    print "install it with pip install blist."
+from threading import Thread, RLock
+from Queue import Queue
+
+
+class EventLoop(Thread):
+    def __init__(self):
+        Thread.__init__(self, target=self._thread_run)
+        self._lock = RLock()
+        self._queue = Queue()
+        # Dict of scheduled times to (function, [category1, ...]) tuples
+        self._scheduled = sorteddict()
+        # Dict of category names to set([time1, time2, ...]) of the times
+        # in _scheduled (as its keys) being used
+        self._categories = {}
+    
+    def run(self, function):
+        self._queue.put(function)
+    
+    def run_external(self, function):
+        pass
+    
+    def schedule(self, function, time):
+        pass
+    
+    def schedule_external(self, function, time):
+        pass
+    
+    def _thread_run(self):
+        pass
+
+
+
+
+
+
+
