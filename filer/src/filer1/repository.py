e7ac3c3f4b269a691520010dfb7448e040c8b2b9
Alexander Boyd (on andromeda) <alex@opengroove.org>
2012-07-24 02:20:12 -0600

    Some ideas, which probably aren't going to work, darnit

--- filer/src/filer1/repository.py
+++ filer/src/filer1/repository.py
@@ -4,65 +4,78 @@ from afn.fileutils import File
 from contextlib import closing
 import hashlib
 from filer1 import exceptions
-import bsdiff4
+import json
 
 def init_repository(folder):
     folder = File(folder)
-    db_file = folder.child(".filerdata")
-    if db_file.exists:
+    filer_dir = folder.child(".filer")
+    if filer_dir.exists:
         raise Exception("There's already a filer repository at that location.")
-    folder.mkdirs(True)
-    # Don't use transactions for now. This is a bad idea and needs to be fixed
-    # in the future.
-    db = sqlite3.connect(db_file.native_path, isolation_level=None)
-    c = db.cursor()
-    c.execute("create table revisions (hash text unique, data text, number integer primary key autoincrement)")
-    c.execute("create table parents (parent text, child text)")
-    db.close()
+    filer_dir.mkdirs(True)
+    # We don't have any sort of transaction support right now. This is a bad
+    # idea and needs to be fixed in the future.
+    
+    # db = sqlite3.connect(db_file.native_path, isolation_level=None)
+    # c = db.cursor()
+    # c.execute("create table revisions (hash text unique, data text, number integer primary key autoincrement)")
+    # c.execute("create table parents (parent text, child text)")
+    # db.close()
 
 
 class Repository(object):
     def __init__(self, folder):
         self.folder = File(folder)
-        self.db_file = self.folder.child(".filerdata")
-        # Don't use transactions for now. This is a bad idea and needs to be
-        # fixed in the future.
-        self.db = sqlite3.connect(self.db_file.native_path, isolation_level=None)
-    
-    def __enter__(self):
-        pass
-    
-    def __exit__(self, *args):
-        self.db.close()
+        self.filer_dir = self.folder.child(".filer")
+        self.revisions = self.filer_dir.child("revisions")
+        self.revisions.mkdirs(True)
+        self.numbers = self.filer_dir.child("numbers")
+        self.numbers.mkdirs(True)
+        # File names are revision hashes, and their contents are their parent
+        # revision hash. This is only one revision right now; in the future,
+        # when merges are supported, this will be a newline-separated list of
+        # parent revisions.
+        self.changeparents = self.filer_dir.child("changeparents")
+        self.changeparents.mkdirs(True)
+        # File names are revision hashes, and their contents are
+        # newline-separated lists of child revisions
+        self.changechildren = self.filer_dir.child("changechildren")
+        self.changechildren.mkdirs(True)
+        # Likewise, but for 
     
     def get_revision(self, id):
         """
         Gets the revision with the specified revision id, which can either be a
         hex string representing the full sha1 hash or the revision's numeric id.
         A ValueError will be thrown if no such revision exists.
+        
+        The return value is a JSON object corresponding to the revision.
+        
+        Note that short revision numbers must still be given as strings.
         """
-        with closing(self.db.cursor()) as c:
-            data = c.execute("select data from revisions where hash = ?", str(id))
-            if data is None:
-                data = c.execute("select data from revisions where number = ?",
-                        int(id))
-            if data is None:
-                raise ValueError("No such revision: %r" % id)
-            return data
+        if self.revisions.child(id).exists:
+            return json.loads(self.revisions.child(id).read())
+        if self.numbers.child(id).exists:
+            return json.loads(self.revisions.child(self.numbers.child(id).read()).read())
     
     def create_revision(self, data):
         """
-        Creates a new revision with the specified data. A (hash, number) tuple
-        will be returned.
+        Creates a new revision with the specified data, which should be a JSON
+        object (not a string). A (hash, number) tuple will be returned.
         """
+        data = json.dumps(data, sort_keys=True)
         hash = hashlib.sha1(data).hexdigest()
-        with closing(self.db.cursor()) as c:
-            c.execute("insert into revisions (hash, data) values (?, ?)",
-                    hash, data)
-            if data["parent"]:
-                c.execute("insert into parents (parent, child) values (?, ?)",
-                        data["parent"], data)
-            return hash, c.lastrowid
+        self.revisions.child(hash).write(data)
+        # TODO: Implement a better algorithm for searching for the next number;
+        # perhaps start at 0 and double until an unused number N is hit, then
+        # do a binary search from 0 to N for the lowest unused number
+        number = 1
+        while self.numbers.child(str(number)).exists:
+            number += 1
+        self.numbers.child(str(number)).write(hash)
+        if data["parent"]:
+            c.execute("insert into parents (parent, child) values (?, ?)",
+                    data["parent"], data)
+        return hash, c.lastrowid
     
     def update_to(self, revision, target, source):
         """
