a15f608fdec3d66f87a6736d24a9fb9a0dfc8e1e
Alexander Boyd <alex@opengroove.org>
2012-07-29 17:25:47 -0600

    Working on new stuff and stuff

--- afn/python/src/afn/utils/simplestatic.py
+++ afn/python/src/afn/utils/simplestatic.py
@@ -26,3 +26,40 @@ I'll probably switch this over to use Python 3's function annotation mechanism
 once I convert this module (and Autobus and everything else that uses them)
 over to Python 3. 
 """
+
+from functools import wraps
+
+def _wrap(function):
+    """
+    Wraps the specified function with a simplestatic type-validating function,
+    then returns this newly-created wrapper. If the specified function is a
+    wrapper previously returned from _wrap, it will be returned as-is.
+    
+    Wrappers have an attribute named simplestatic whose value is a dictionary;
+    this dictionary can be modified to alter the type-validating behavior of
+    the wrapper. Three keys can be present; params, remainder, and return.
+    params is a list (or other sequence) of types to validate each parameter
+    against. remainder is a type to apply to any additional parameters; this is
+    typically used for variable argument functions.
+    """
+    if hasattr(function, "simplestatic"):
+        return function
+    simplestatic = {}
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        return function(*args, **kwargs)
+    wrapper.simplestatic = simplestatic
+    return wrapper
+
+
+
+
+
+
+
+
+
+
+
+
+
