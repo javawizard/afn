770862255f6eec8e13bacdeed5be2357d8e4d393
Alexander Boyd <alex@opengroove.org>
2012-08-05 19:44:54 -0600

    Working on a range file, but I think I'm going to make this not as general and only work as a stream for reading a particular piece of binary data.

--- filer/src/filer1/bec.py
+++ filer/src/filer1/bec.py
@@ -5,3 +5,46 @@ of data (think gigabytes) to be stored in JSON objects without running out of
 memory.
 """
 
+class RangeFile(object):
+    def __init__(self, file, offset, length, read_only):
+        self.file = file
+        self.offset = offset
+        self.length = length
+        self.read_only = read_only
+    
+    def close(self):
+        """
+        Does nothing; close is a no-op on range files. The underlying file must
+        be opened and closed separately.
+        """
+    
+    def __enter__(self):
+        """
+        Method to allow RangeFile instances to be used as ordinary files; the
+        implementation does nothing.
+        """
+    
+    def __exit__(self, *args):
+        """
+        Ditto from __enter__. Does nothing; see self.close for why.
+        """
+    
+    def __flush__(self, *args):
+        """
+        Flushes the underlying file.
+        """
+        self.file.flush()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
