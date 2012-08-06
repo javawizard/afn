d1094f7144e7136e5e4b8adc734722c6826c9b82
Alexander Boyd <alex@opengroove.org>
2012-08-05 19:36:56 -0600

    On second thought, I'll just leave it in Filer for now.

---
+++ filer/src/filer1/bec.py
@@ -0,0 +1,7 @@
+"""
+BEC: Binary Encoding, Canonicalized: A JSON encoding, and corresponding
+library, for storing JSON in a canonical format, and for allowing large amounts
+of data (think gigabytes) to be stored in JSON objects without running out of
+memory.
+"""
+
