dfe3278c88ab64d76993ec9973eba44b94b6209f
Alexander Boyd <alex@opengroove.org>
2012-08-05 20:32:32 -0600

    Stuff

--- filer/src/filer1/bec.py
+++ filer/src/filer1/bec.py
@@ -8,8 +8,18 @@ memory.
 from collections import MutableMapping, Mapping, Sequence
 from cStringIO import StringIO
 from filer1 import exceptions
+from os import SEEK_SET, SEEK_CUR, SEEK_END
+import struct
 
 JSON_TYPES = (int, long, float, basestring, bool, type(None), Sequence, Mapping)
+
+_DICT = 1
+_LIST = 2
+_BYTES = 3
+_NUMBER = 4
+_BOOL = 5
+_NULL = 6
+
 
 class BECDict(object):
     def __init__(self):
@@ -57,6 +67,62 @@ class BECDict(object):
         return self._data.__contains__(key)
 
 
+class BECStream(object):
+    def __init__(self, file, offset, length):
+        self.file = file
+        self.offset = offset
+        self.length = length
+        self.position = 0
+    
+    def close(self):
+        pass
+    
+    def flush(self):
+        pass
+    
+    def next(self):
+        raise NotImplementedError
+    
+    def read(self, size=None):
+        # Seek to our current position
+        self.file.seek(self.offset + self.position)
+        # Figure out how many bytes left before the end of the file
+        remaining = self.length - self.position
+        # Trim the size down to the maximum bytes remaining
+        if size is None or size > remaining:
+            size = remaining
+        # Add the number of bytes to our position
+        self.position += size
+        # Then read and return the bytes.
+        return self.file.read(size)
+    
+    def seek(self, offset, whence=SEEK_SET):
+        new_position = self.position
+        if whence == SEEK_SET: # Absolute seeking; add self.offset first
+            new_position = self.offset + offset
+        elif whence == SEEK_CUR: # Relative seeking
+            new_position += offset
+        else: # Assume SEEK_END; TODO: might want to check explicitly
+            new_position = self.offset + self.length + offset
+        # Trim position to file boundaries
+        new_position = max(min(new_position, self.offset + self.length), self.offset)
+        # Then update our position
+        self.position = new_position
+    
+    def tell(self):
+        return self.position
+    
+    # TODO: need to implement readline, readlines, and next
+
+
+def _load_at(file, position):
+    file.seek(position)
+    # First byte is type, next eight bytes are length
+    value_type = file.read(1)
+    value_length = struct.unpack("q", file.read(8))
+    if value_type == 
+
+
 def load(file, at_current=False):
     """
     Loads the specified BEC file. The file will be left seeked to just after
@@ -71,6 +137,9 @@ def load(file, at_current=False):
     one after the other, to the same file with bec.dump, and then loaded in
     turn with bec.load(..., at_current=True).
     """
+    if not at_current:
+        file.seek(0)
+    return _load_at(file, file.tell())
 
 
 def dump(value, file):
