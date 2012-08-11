9bb10bc1978e438b20357dacdb73196b7109ade7
Alexander Boyd <alex@opengroove.org>
2012-08-10 21:53:20 -0600

    Stuff. Probably going to refactor the archiver to archive things into separate files right now and then combine them together after archiving is done. That'll ensure things can be written to the archive in the proper order even when multiple archiving threads are used.

--- filer/src/filer1/archive.py
+++ filer/src/filer1/archive.py
@@ -7,6 +7,7 @@ import gzip
 import shutil
 from afn.fileutils import File
 from filer1 import bec
+from afn.utils.concurrent import AtomicInteger
 
 LITERAL = "\x01"
 LITERAL_GZIP = "\x02"
@@ -40,26 +41,28 @@ def make_archive(objects, out, temp_dir):
     """
     # out.write(MAGIC)
     window = []
+    current = AtomicInteger(0)
     in_lock = RLock()
     out_lock = RLock()
     # TODO: Spawn several processor threads
-    in_total, out_total = _process(objects, window, in_lock, out_lock, temp_dir)
+    in_total, out_total = _process(objects, window, current, in_lock, out_lock, temp_dir)
     print "Total space savings: %s%%" % int(100*(1.0 - (float(out_total) / float(in_total))))
+    print "Input size: %sKB, output size: %sKB" % (in_total / 1000, out_total / 1000)
 
 
-def _process(object_generator, window, in_lock, out_lock, temp_dir):
+def _process(objects, window, current, in_lock, out_lock, temp_dir):
     in_total, out_total, processed = 0, 0, 0
     while True:
         with in_lock:
-            try:
-                hash, object_file = object_generator.next()
-            except StopIteration:
+            position = current.get_and_add()
+            if position >= len(objects):
                 return in_total, out_total
+            hash, object_file = objects[position]
             # Make a copy /before/ appending to ourselves so as not to
             # accidentally diff against ourselves
             window_copy = list(window)
             window.append((hash, object_file))
-            if len(window) > 150:
+            if len(window) > 10:
                 del window[0]
         o_dir = temp_dir.child(hash)
         o_dir.mkdirs()
