d8057e17db7ddb330915f76c65b72091880df3d3
Alexander Boyd <alex@opengroove.org>
2012-09-18 05:08:48 -0600

    Starting work on repository stuff; so far I've got a function for reading object headers. (Objects are going to be structured much differently in filer-hs than in filer-python.)

--- filer-hs/src/Filer/Repository.hs
+++ filer-hs/src/Filer/Repository.hs
@@ -10,8 +10,30 @@ data Repository = Repository FilePath
 
 data ObjectType = Blob | Tree | Commit | Changeset
 
+data ObjectHeader = ObjectHeader ObjectType
+
 
 getRepoPath (Repository path) = path
+
+makeMagic :: String -> [Word8]
+makeMagic = unpack . fromString
+
+magicBlob = makeMagic "filerblb"
+magicTree = makeMagic "filertre"
+magicCommit = makeMagic "filercmt"
+magicChangeset = makeMagic "filercst"
+
+magicLength = 8
+
+readObjectHeader :: Handle -> IO ObjectHeader
+readObjectHeader handle = do
+    headerData = hGet handle magicLength
+    case headerData of
+        magicBlob      -> return $ ObjectHeader Blob
+        magicTree      -> return $ ObjectHeader Tree
+        magicCommit    -> return $ ObjectHeader Commit
+        magicChangeset -> return $ ObjectHeader Changeset
+        _              -> error ("Invalid object header: " ++ headerData)  
 
 
 
