85ae0630fa3892e76e1b56d30c13ef63fcce67e5
Alexander Boyd <alex@opengroove.org>
2012-09-20 13:07:15 -0600

    Restructured the data model of the graph database

--- filer-hs/src/Filer/Graph.hs
+++ filer-hs/src/Filer/Graph.hs
@@ -10,9 +10,27 @@ type DataMap = Map String B.ByteString
 
 data DB = DB FilePath
 
-data Ref = Ref String Hash DataMap
+-- An Info has a type, which is a string, and a map of additional attributes
+-- present on the info. Every object has an info and every ref has an info.
+-- For example, folders are stored as an object with an info whose type is
+-- "folder", and which has a number of refs, each one's type being "child" and
+-- with a single attribute named "name" whose value is the name of the file
+-- under which the referred commit is to be stored.
+data Info = Info String DataMap
 
-data Object = Object String DataMap [Ref]
+-- A ref is a pointer to an object, along with a type and some associated
+-- attributes. Every object has zero or more refs.
+data Ref = Ref Hash Info
+
+-- An object is an entry in the graph database. It is uniquely identified by
+-- its SHA-256 hash, and it contains a type, zero or more attributes, and zero
+-- or more refs.
+data Object = Object Info [Ref]
+
+{-
+So we need a data format...
+
+-}
 
 
 readObject :: DB -> Hash -> 
