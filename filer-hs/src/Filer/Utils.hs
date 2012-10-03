904adff055343a69b190b807753982fa39830373
Alexander Boyd <alex@opengroove.org>
2012-10-02 22:36:28 -0600

    Added strictToLazy :: Data.ByteString.ByteString -> Data.ByteString.Lazy.ByteString and lazyToStrict :: Data.ByteString.Lazy.ByteString -> Data.ByteString.ByteString

--- filer-hs/src/Filer/Utils.hs
+++ filer-hs/src/Filer/Utils.hs
@@ -2,6 +2,8 @@
 module Filer.Utils where
 
 import Data.Word
+import Data.ByteString
+import Data.ByteString.Lazy
 
 
 translateEnum :: (Enum a, Enum b) => a -> b
@@ -9,5 +11,11 @@ translateEnum = toEnum . fromEnum
 
 translateList :: (Enum a, Enum b) => [a] -> [b]
 translateList = map translateEnum
+
+strictToLazy :: Data.ByteString.ByteString -> Data.ByteString.Lazy.ByteString
+strictToLazy = Data.ByteString.Lazy.fromChunks . (:[])
+
+lazyToStrict :: Data.ByteString.Lazy.ByteString -> Data.ByteString.ByteString
+lazyToStrict = Data.ByteString.concat . Data.ByteString.Lazy.toChunks
 
 
