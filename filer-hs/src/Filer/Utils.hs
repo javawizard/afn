51d5ac2be7ea86f72bf0c4fa25aa873cf2d2ac58
Alexander Boyd <alex@opengroove.org>
2012-10-04 00:11:14 -0600

    Wrote whenM :: Monad m => m Bool -> m () -> m ()

--- filer-hs/src/Filer/Utils.hs
+++ filer-hs/src/Filer/Utils.hs
@@ -4,6 +4,7 @@ module Filer.Utils where
 import Data.Word
 import Data.ByteString
 import Data.ByteString.Lazy
+import Control.Monad (when)
 
 
 translateEnum :: (Enum a, Enum b) => a -> b
@@ -17,5 +18,10 @@ strictToLazy = Data.ByteString.Lazy.fromChunks . (:[])
 
 lazyToStrict :: Data.ByteString.Lazy.ByteString -> Data.ByteString.ByteString
 lazyToStrict = Data.ByteString.concat . Data.ByteString.Lazy.toChunks
+
+whenM :: Monad m => m Bool -> m () -> m ()
+whenM test ifTrue = do
+    value <- test
+    when test ifTrue
 
 
