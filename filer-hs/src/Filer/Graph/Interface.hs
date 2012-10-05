e23b74a8743be2b8e102205ffdf3cf52b57c1776
Alexander Boyd (on phenoxydine) <alex@opengroove.org>
2012-10-04 21:35:06 -0600

    Documented the query data types

--- filer-hs/src/Filer/Graph/Interface.hs
+++ filer-hs/src/Filer/Graph/Interface.hs
@@ -5,25 +5,47 @@ import qualified Data.ByteString.Lazy as B
 import qualified Data.Map as M
 import qualified Data.Set as S
 
+-- | A query looking at references between objects
 data RefQuery
+    -- | Matches a ref if the attribute query matches any of the
+    -- ref's attributes
     = RefHasAttributes AttributeQuery
+    -- | Matches a ref if it points to an object matching the specified query
     | PointsTo ObjectQuery
+    -- | Matches a ref if it points from an object matching the specified query
     | PointsFrom ObjectQuery
+    -- | Matches a ref if both queries match
     | AndR RefQuery RefQuery
+    -- | Matches a ref if either query matches
     | OrR RefQuery RefQuery
+    -- | Matches a ref if the specified query does not match
     | NotR RefQuery
+-- | A query looking at attributes on an object or on a ref
 data AttributeQuery
+    -- | Matches if the specified attribute exists, regardless of its value
     = HasAttribute String
+    -- | Matches if the specified attribute exists with the specified value
     | HasValue String ByteString
+    -- | Matches if both queries match
     | AndA AttributeQuery AttributeQuery
+    -- | Matches if either query matches
     | OrA AttributeQuery AttributeQuery
+    -- | Matches if the specified query does not match
     | NotA AttributeQuery
-data ObjectQuery 
+-- | A query looking at objects.
+data ObjectQuery
+    -- | Matches if the object has a matching ref pointing to it
     = HasIncomingRef RefQuery
+    -- | Matches if the object has a matching ref pointing from it
     | HasOutgoingRef RefQuery
+    -- | Matches an object if the attribute query matches any of the object's
+    -- attributes
     | ObjectHasAttributes AttributeQuery
+    -- | Matches an object if both queries match
     | AndO ObjectQuery ObjectQuery
+    -- | Matches an object if either query matches
     | OrO ObjectQuery ObjectQuery
+    -- | Matches an object if the specified query does not match
     | NotO ObjectQuery
 
 -- | An alias for Data.Map.Map String Data.ByteString.Lazy.ByteString used to
@@ -51,7 +73,7 @@ class ReadDB a where
 -- to allow for efficient querying.
 class QueryDB a where
     -- | Gets the hashes of all objects that match the specified query.
-    query :: a -> Query -> IO [Hash]
+    query :: a -> ObjectQuery -> IO [Hash]
 
 -- | Graph databases that can be written to.
 class WriteDB a where
