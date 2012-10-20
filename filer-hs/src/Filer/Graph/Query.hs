cc40bfc6b5f293ef6df61f8c2aacd7ad5502fe20
Alexander Boyd <alex@opengroove.org>
2012-10-20 17:24:05 -0600

    Moving query-related things to the Query module, and adding IntInRange a b which will convert to slightly more efficient SQL than OrV (IntValue $ IntGreaterThan (a-1)) (IntValue $ IntLessThan (b+1))

---
+++ filer-hs/src/Filer/Graph/Query.hs
@@ -0,0 +1,70 @@
+
+module Filer.Graph.Query where
+import Data.ByteString.Lazy (ByteString)
+
+data ValueQuery
+    = IntValue IntQuery
+    | BoolValue BoolQuery
+    | StringValue StringQuery
+    | BinaryValue BinaryQuery
+    | AndV ValueQuery ValueQuery
+    | OrV ValueQuery ValueQuery
+    | NotV ValueQuery ValueQuery
+    | AnyValue
+
+data IntQuery
+    = IntGreaterThan Integer
+    | IntLessThan Integer
+    -- | First int is min, second int is max
+    | IntInRange Integer Integer
+    | IntEqualTo Integer
+    | AnyInt
+
+data BoolQuery
+    = BoolEqualTo Bool
+    | AnyBool
+
+data StringQuery
+    = StringEqualTo String
+    -- TODO: Add additional things for seeing if the string in question is
+    -- contained within an attribute's value or things like that, or maybe even
+    -- support regexes
+    | AnyString
+
+data BinaryQuery
+    = BinaryEqualTo ByteString
+    -- TODO: Add length queries, so that we can search for attributes with
+    -- large values
+    | AnyBinary
+
+data RefQuery
+    = RefHasAttributes AttributeQuery
+    | PointsTo ObjectQuery
+    | PointsFrom ObjectQuery
+    | AndR RefQuery RefQuery
+    | OrR RefQuery RefQuery
+    | NotR RefQuery
+
+data AttributeQuery
+    = HasAttribute String ValueQuery
+    | AndA AttributeQuery AttributeQuery
+    | OrA AttributeQuery AttributeQuery
+    | NotA AttributeQuery
+
+data ObjectQuery
+    = HasIncomingRef RefQuery
+    | HasOutgoingRef RefQuery
+    | ObjectHasAttributes AttributeQuery
+    | HashIs Hash
+    | AndO ObjectQuery ObjectQuery
+    | OrO ObjectQuery ObjectQuery
+    | NotO ObjectQuery
+
+    
+    
+    
+    
+    
+    
+    
+    
