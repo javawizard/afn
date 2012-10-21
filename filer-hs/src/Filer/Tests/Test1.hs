c7262f621ac431a41ea24e9b5e45298037cbcb9d
Alexander Boyd <alex@opengroove.org>
2012-10-21 00:34:57 -0600

    Add multiple objects, with refs

--- filer-hs/src/Filer/Tests/Test1.hs
+++ filer-hs/src/Filer/Tests/Test1.hs
@@ -12,6 +12,7 @@ import qualified Data.Set as S
 main = do
     sqldb <- connectSqlite3 "/home/jcp/test/sqlite/hashdb.sql"
     db <- connect sqldb
-    addObject db (M.fromList [("a", StringValue "b"), ("c", StringValue "d")]) S.empty
+    hash1 <- addObject db (M.fromList [("a", StringValue "b"), ("c", StringValue "d")]) S.empty
+    hash2 <- addObject db (M.fromList [("e", IntValue 42)] S.fromList [(hash1, M.fromList [("f", IntValue 45)])]
     commit sqldb
     putStrLn "It worked!"
