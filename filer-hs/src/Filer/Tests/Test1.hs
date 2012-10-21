e78f82c861ae6bee0ee7784ecc283ccb16e6b46d
Alexander Boyd <alex@opengroove.org>
2012-10-21 01:36:55 -0600

    Read objects back now

--- filer-hs/src/Filer/Tests/Test1.hs
+++ filer-hs/src/Filer/Tests/Test1.hs
@@ -14,5 +14,10 @@ main = do
     db <- connect sqldb
     hash1 <- addObject db (M.fromList [("a", StringValue "b"), ("c", StringValue "d")]) S.empty
     hash2 <- addObject db (M.fromList [("e", IntValue 42)]) $ S.fromList [(hash1, M.fromList [("f", IntValue 45)])]
+    putStrLn "Two objects written. Number of objects in the database:"
+    getObjectCount db >>= putStrLn . show
+    putStrLn "About to read all objects back"
+    getAllHashes db >>= mapM_ (\h -> getObject h >>= putStrLn . show)
+    putStrLn "All objects read. About to commit" 
     commit sqldb
-    putStrLn "It worked!"
+    putStrLn "Done!"
