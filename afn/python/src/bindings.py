846a9cbee294a988b0a157f80ee17171ff9939f2
Alexander Boyd <alex@opengroove.org>
2012-11-09 11:40:01 -0700

    Working on circuits

--- afn/python/src/bindings.py
+++ afn/python/src/bindings.py
@@ -1,18 +1,46 @@
 
 from collections import namedtuple
+from threading import local as Local
 
 ValueSet = namedtuple("ValueSet", ["value"])
 
-def circuit():
+context_thread_local = Local()
+
+class CircuitContext(object):
+    """
+    A context manager that, upon entering, sets the current circuit to a
+    newly-created circuit and pushes the formerly-current circuit onto a stack.
+    Upon exiting, the former circuit will be restored, if there was any.
+    """
+    def __enter__(self):
+        stack = context_thread_local.__dict__.setdefault("circuit_stack", [])
+        stack.append(set())
+    
+    def __exit__(self):
+        stack = context_thread_local.circuit_stack
+        del stack[-1]
+        if not stack:
+            del context_thread_local.circuit_stack
+
+
+class ProcessingContext(object):
+    """
+    A context manager that, upon entering, 
+    """
     pass
+
+
+circuit = CircuitContext
+processing = ProcessingContext
 
 
 def processed(object):
-    pass
-
-
-def processing(object):
-    pass
+    """
+    Returns true if the specified object has been processed in the current
+    circuit, or false if it hasn't. Note that this will throw an exception if
+    there isn't a current circuit. (It's considered an error to invoke
+    validate or receive without a circuit, so this is usually what you want.)
+    """
 
 
 class ValueSender(object):
