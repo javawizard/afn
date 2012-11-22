8ed9b3843badb97bab7dbf94f51304dff6d72e1b
Alexander Boyd <alex@opengroove.org>
2012-11-22 03:23:07 -0700

    A few bugfixes. Things compile now!

--- landfill-hs-public/src/CreditUnion/Client.hs
+++ landfill-hs-public/src/CreditUnion/Client.hs
@@ -1,18 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module CreditUnion.Client where
 
 import Control.Monad.Trans (liftIO)
 import Test.WebDriver
 import Test.WebDriver.Commands
 import Data.Text (pack, unpack)
+import Data.List (isInfixOf)
 
 login :: String -> [(String, String)] -> String -> WD ()
 login username questions password = do
     openPage "https://my.ucreditu.com/"
-    findElem $ ById "UsernameField" >>= sendKeys $ pack username
-    findElem $ ById "SubmitNext" >>= click
-    findElem $ ById "PasswordField" >>= sendKeys $ pack password
-    findElem $ ById "SubmitNext" >>= click
-    bodyText <- findElem $ ByCSS "body" >>= getText
+    findElem (ById "UsernameField") >>= (sendKeys $ pack username)
+    findElem (ById "SubmitNext") >>= click
+    findElem (ById "PasswordField") >>= (sendKeys $ pack password)
+    findElem (ById "SubmitNext") >>= click
+    bodyText <- findElem (ByCSS "body") >>= getText >>= return . unpack
     let (_, answer) = filter (\q -> fst q `isInfixOf` bodyText) questions !! 0
-    findElem $ ById "Answer" >>= sendKeys $ pack answer
-    findElem $ ById "SubmitNext" >>= click
+    findElem (ById "Answer") >>= (sendKeys $ pack answer)
+    findElem (ById "SubmitNext") >>= click
 
