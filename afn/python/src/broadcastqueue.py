9b9043be8ef6e03df52e3866c20aa0dad220df17
Alexander Boyd <alex@opengroove.org>
2013-01-07 22:18:08 -0700

    Module docs

--- afn/python/src/broadcastqueue.py
+++ afn/python/src/broadcastqueue.py
@@ -19,17 +19,30 @@ class _Link(object):
 
 
 class BroadcastQueue(object):
+    """
+    A queue-like object that allows multiple "endpoints", each of which
+    provides separate read access to the queue. Whenever an item is added to
+    the queue, it becomes available at every endpoint separately.
+    """
     def __init__(self):
         self.condition = Condition()
         self._link = _Link()
     
     def put(self, value):
+        """
+        Inserts an item into this queue.
+        """
         with self.condition:
             item = _Item(value, _Link())
             self._link.item = item
             self._link = item.link
     
     def new_endpoint(self):
+        """
+        Creates a new endpoint for reading from this queue. The endpoint
+        initially starts out empty; new items will be available from it once
+        Queue.put is called.
+        """
         return _Endpoint(self, self._link)
 
 
@@ -44,6 +57,13 @@ class _Endpoint(object):
         self._link = link
     
     def get(self):
+        """
+        Returns the next item available at the endpoint, or raises Empty if no
+        items are available.
+        
+        In the future (read: in a few hours), this will be able to block if no
+        items are available.
+        """
         with self._queue.condition:
             if self._link.item:
                 value = self._link.item.value
