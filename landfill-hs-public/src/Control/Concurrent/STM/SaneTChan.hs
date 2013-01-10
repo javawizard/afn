679310fbf0c26eec75507733237774126b3a1800
Alexander Boyd <alex@opengroove.org>
2013-01-10 00:48:30 -0700

    Writing a sane version of Control.Concurrent.STM.TChan, with the read ends and the write ends separated

---
+++ landfill-hs-public/src/Control/Concurrent/STM/SaneTChan.hs
@@ -0,0 +1,40 @@
+
+import Control.Concurrent
+import Control.Concurrent.STM
+
+-- readTVar, writeTVar, atomically, retry, orElse
+
+
+data TChan a = TChan (TVar Link)
+data TPort a = TPort (TVar Link)
+
+type Item a = Empty | Item a (Link a)
+type Link a = TVar (Item a)
+
+newTChan :: STM (TChan a)
+newTChan = do
+    link <- newTVar Empty
+    chanVar <- newTVar link
+    return $ TChan chanVar
+
+writeTChan :: TChan a -> a -> STM ()
+writeTChan (TChan chanVar) item = do
+    newLink <- newTVar Empty
+    let itemWrapper = Item item newLink
+    currentLink <- readTVar chanVar
+    writeTVar currentLink itemWrapper
+    writeTVar chanVar newLink
+
+newTPort :: TChan a -> STM (TPort a)
+newTPort (TChan chanVar) = do
+    currentLink <- readTVar chanVar
+    portVar <- newTVar currentLink
+    return $ TPort portVar
+
+
+
+
+
+
+
+
