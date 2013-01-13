83e3e637542e621e864e6091c36158abc26ac2cc
Alexander Boyd <alex@opengroove.org>
2013-01-13 00:50:03 -0700

    streamSocket is done

--- landfill-hs-public/src/Zelden/IO.hs
+++ landfill-hs-public/src/Zelden/IO.hs
@@ -7,5 +7,36 @@ import Control.Concurrent.STM.SaneTChan
 import Network
 
 streamSocket :: Handle -> (String -> i) -> (o -> String) -> Queue (Maybe i) -> Endpoint (Maybe o) -> IO ()
-
+streamSocket handle inputConverter outputConverter inputQueue outputEndpoint = do
+    -- Start input thread
+    forkIO $ do
+        let doneWithSocket = do
+            -- Send Nothing to the input queue
+            writeQueue inputQueue Nothing
+            -- Close the handle, just in case
+            hClose handle
+            return Nothing
+        let process = do
+            -- Read a line from the socket, calling doneWithSocket on errors
+            line <- catch (liftM Just $ hGetLine handle) $ const doneWithSocket
+            case line of
+                Nothing -> return ()
+                Just l -> do
+                    atomically $ writeQueue inputQueue $ inputConverter l
+                    process
+        process
+    -- Start output thread
+    forkIO $ do
+        let process = do
+            -- Read the next item from the endpoint
+            nextItem <- atomically $ readEndpoint outputEndpoint
+            case nextItem of
+                -- If it's Nothing, close the socket
+                Nothing -> hClose handle
+                -- Otherwise, write the (converted) message out and read another
+                -- item from the endpoint
+                Just m -> do
+                    hPutStrLn $ outputConverter m
+                    process
+        process
 
