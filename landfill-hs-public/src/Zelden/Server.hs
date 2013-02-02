e1ede9a563dab1fb0bf84f74d9df90a719cb586d
Alexander Boyd <alex@opengroove.org>
2013-02-02 13:18:49 -0700

    Working on events

--- landfill-hs-public/src/Zelden/Server.hs
+++ landfill-hs-public/src/Zelden/Server.hs
@@ -3,18 +3,40 @@ module Zelden.Server where
 
 import Zelden.IO
 import Database.HDBC as DB
+import qualified Data.Map as M
 
 
 data ConnectionBox = ConnectionBox (forall a. Connection a => a)
 data ProtocolBox = ProtocolBox (forall a. Protocol a => a)
 
-data Event
+
+data Event = Event EventData (M.Map String String)
+type RoomKey = String
+type UserKey = String
+
+data EventData
     = Connected
     | Disconnected
-    | MessageReceived MessageInfo
-    | JoinedChannel String
-    | LeftChannel String
-    -- Add lots more events and stuff
+    | JoinedRoom RoomKey
+    | PartedRoom RoomKey
+    | RoomTopic RoomKey String
+    | RoomMessage RoomKey UserKey String
+    | UserMessage UserKey String
+    | UserStatus UserKey UserStatus
+
+data UserStatus
+    = Available
+    | Away
+    | Busy
+    | Offline
+
+-- | Alias for (,) that allows conveniently writing dictionaries as
+-- M.fromList ["a" := "b", "c" := "d"].
+(:=) :: a -> b -> (a, b)
+(:=) = (,)
+infixl 1 :=
+
+
 
 type EventCallback = ConnectionBox -> Event -> IO ()
 
@@ -28,6 +50,11 @@ class Connection a where
     getProtocol :: a -> ProtocolBox
     connect :: a -> IO ()
     sendMessage :: a
+
+-- | A class of types that can receive events from connections. Connections are
+-- given a Controller to which they indicate events that happen.
+class Controller a where
+    
 
 
 
