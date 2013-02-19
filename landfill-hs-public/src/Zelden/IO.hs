b584488aea6a216156fcd6554a237186f091cd03
Alexander Boyd <alex@opengroove.org>
2013-02-18 21:49:44 -0700

    Debug statements

--- landfill-hs-public/src/Zelden/IO.hs
+++ landfill-hs-public/src/Zelden/IO.hs
@@ -13,38 +13,53 @@ streamSocket handle inputConverter outputConverter inputQueue outputEndpoint = d
     -- Start input thread
     forkIO $ do
         let doneWithSocket = do
+            putStrLn "1"
             -- Send Nothing to the input queue
             atomically $ writeQueue inputQueue Nothing
+            putStrLn "2"
             -- Close the handle, just in case
             hClose handle
+            putStrLn "3"
             return Nothing
         let process = do
             -- Read a line from the socket, calling doneWithSocket on errors
+            putStrLn "4"
             line <- catch (liftM Just $ hGetLine handle) $ const doneWithSocket
+            putStrLn "5"
             case line of
-                Nothing -> return ()
+                Nothing -> putStrLn "6" >> return ()
                 Just l -> do
+                    putStrLn "7"
                     case inputConverter l of
-                        Nothing -> return ()
-                        Just thing -> atomically $ writeQueue inputQueue $ Just thing 
+                        Nothing -> putStrLn "8" >> return ()
+                        Just thing -> putStrLn "9" >> (atomically $ writeQueue inputQueue $ Just thing)
+                    putStrLn "10" 
                     process
+        putStrLn "11"
         process
     -- Start output thread
     forkIO $ do
+        putStrLn "12"
         let process = do
+            putStrLn "13"
             -- Read the next item from the endpoint
             nextItem <- atomically $ readEndpoint outputEndpoint
+            putStrLn "14"
             case nextItem of
                 -- If it's Nothing, close the socket
-                Nothing -> hClose handle
+                Nothing -> putStrLn "15" >> hClose handle
                 -- Otherwise, write the (converted) message out and read another
                 -- item from the endpoint
                 Just m -> do
+                    putStrLn "16"
                     case outputConverter m of
-                        Nothing -> return ()
-                        Just thing -> hPutStrLn handle thing 
+                        Nothing -> putStrLn "17" >> return ()
+                        Just thing -> putStrLn "18" >> hPutStrLn handle thing
+                    putStrLn "19" 
                     process
+        putStrLn "20"
         process
+    putStrLn "21"
     return ()
 
 streamSocket' :: Handle -> (String -> Maybe i) -> (o -> Maybe String) -> IO (Endpoint (Maybe i), Queue (Maybe o))
