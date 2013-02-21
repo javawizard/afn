b26a7970d6d22f5b17983ffc9d3dbd8bac98c9cd
Alexander Boyd <alex@opengroove.org>
2013-02-21 00:33:12 -0700

    Bugfixes. Disconnects now work properly.

--- landfill-hs-public/src/Zelden/IO.hs
+++ landfill-hs-public/src/Zelden/IO.hs
@@ -2,12 +2,13 @@
 module Zelden.IO where
 
 import System.IO
-import Control.Monad (liftM)
+import Control.Monad (liftM, when)
 import Control.Concurrent
 import Control.Concurrent.STM
 import Control.Concurrent.STM.SaneTChan
 import Network
 import Network.Socket
+-- import Network.BSD
 
 streamSocket :: Socket -> (String -> Maybe i) -> (o -> Maybe String) -> Queue (Maybe i) -> Endpoint (Maybe o) -> IO ()
 streamSocket socket inputConverter outputConverter inputQueue outputEndpoint = do
@@ -15,53 +16,38 @@ streamSocket socket inputConverter outputConverter inputQueue outputEndpoint = d
     -- Start input thread
     forkIO $ do
         let doneWithSocket = do
-            putStrLn "1"
             -- Send Nothing to the input queue
             atomically $ writeQueue inputQueue Nothing
-            putStrLn "2"
             -- Close the handle, just in case
-            hClose handle
-            putStrLn "3"
+            catch (sClose socket) $ const $ return ()
             return Nothing
         let process = do
             -- Read a line from the socket, calling doneWithSocket on errors
-            putStrLn "4"
             line <- catch (liftM Just $ getLineFrom lineSocket) $ const doneWithSocket
-            putStrLn "5"
             case line of
-                Nothing -> putStrLn "6" >> return ()
+                Nothing -> return ()
                 Just l -> do
-                    putStrLn "7"
                     case inputConverter l of
-                        Nothing -> putStrLn "8" >> return ()
-                        Just thing -> putStrLn "9" >> (atomically $ writeQueue inputQueue $ Just thing)
-                    putStrLn "10" 
+                        Nothing -> return ()
+                        Just thing -> (atomically $ writeQueue inputQueue $ Just thing)
                     process
-        putStrLn "11"
         process
     -- Start output thread
     forkIO $ do
-        putStrLn "12"
         let process = do
-            putStrLn "13"
             -- Read the next item from the endpoint
             nextItem <- atomically $ readEndpoint outputEndpoint
-            putStrLn "14"
             case nextItem of
                 -- If it's Nothing, close the socket
-                Nothing -> putStrLn "15" >> sClose socket
+                Nothing -> (catch (sClose socket) $ const $ return ())
                 -- Otherwise, write the (converted) message out and read another
                 -- item from the endpoint
                 Just m -> do
-                    putStrLn "16"
                     case outputConverter m of
-                        Nothing -> putStrLn "17" >> return ()
-                        Just thing -> putStrLn "18" >> sendAll socket thing
-                    putStrLn "19" 
+                        Nothing -> return ()
+                        Just thing -> sendAll socket (thing ++ "\n")
                     process
-        putStrLn "20"
         process
-    putStrLn "21"
     return ()
 
 streamSocket' :: Socket -> (String -> Maybe i) -> (o -> Maybe String) -> IO (Endpoint (Maybe i), Queue (Maybe o))
@@ -85,9 +71,9 @@ newLineSocket s = do
 getLineFrom :: LineSocket -> IO String
 getLineFrom lineSocket@(LineSocket socket var) = do
     value <- atomically $ readTVar var
-    if "\n" `elem` value
+    if '\n' `elem` value
         then do
-            let (result, _:rest) = break (== "\n") value
+            let (result, _:rest) = break (== '\n') value
             atomically $ writeTVar var rest
             return result
         else do
