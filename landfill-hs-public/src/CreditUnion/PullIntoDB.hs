2181bb643d2e6b9c5fdcaa41e6325151e220bb1e
Alexander Boyd <alex@opengroove.org>
2013-03-01 22:41:24 -0700

    Stuff

--- landfill-hs-public/src/CreditUnion/PullIntoDB.hs
+++ landfill-hs-public/src/CreditUnion/PullIntoDB.hs
@@ -9,22 +9,30 @@ import Control.Monad.Trans (liftIO)
 import Database.HDBC
 import Database.HDBC.Sqlite3
 import System (getArgs)
+import Data.Time.Clock.POSIX
 
 main = do
     args <- getArgs
     db <- connectSqlite3 $ args !! 0
+    run db "create table if not exists balance (person text, shareid text, sharename text, when integer, available integer, total integer)" []
+    commit db
     password <- getPass'
     questions <- liftM read $ readFile "/home/jcp/ucreditu/questions"
     putStrLn "Running."
     let loop = do
-        putStrLn "Starting session"
-        accounts <- runSession defaultSession defaultCaps $ do
-            login "javawizard" questions pass
-            accounts <- getAccounts
-            logout
-            return accounts
-        putStrLn "Inserting into database"
-        
-        putStrLn "Done"
-        threadDelay (1800*1000*1000)
+        flip catch (\e -> putStrLn $ "Exception happened: " ++ show e) $ do
+            putStrLn "Starting session"
+            accounts <- runSession defaultSession defaultCaps $ do
+                login "javawizard" questions pass
+                accounts <- getAccounts
+                logout
+                return accounts
+            putStrLn "Inserting into database"
+            time <- liftM (floor . realToFrac) getPOSIXTime
+            forM_ accounts $ \(shareName, _, number, available, total) -> do
+                
+            commit db 
+            putStrLn "Done"
+        threadDelay (3600*1000*1000)
         loop
+    loop
