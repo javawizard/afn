72e6df67b053841baa2c63749240133169251341
Alexander Boyd <alex@opengroove.org>
2013-03-02 02:43:58 -0700

    Stuff

--- landfill-hs-public/src/CreditUnion/PullIntoDB.hs
+++ landfill-hs-public/src/CreditUnion/PullIntoDB.hs
@@ -8,14 +8,16 @@ import Test.WebDriver
 import Control.Monad.Trans (liftIO)
 import Database.HDBC
 import Database.HDBC.Sqlite3
-import System (getArgs)
+import System.Environment (getArgs)
 import Data.Time.Clock.POSIX
 import System.Process (system)
+import Control.Concurrent
+import Control.Monad
 
 main = do
     args <- getArgs
     db <- connectSqlite3 $ args !! 0
-    run db "create table if not exists balance (person text, shareid text, sharename text, when integer, available integer, total integer)" []
+    run db "create table if not exists balance (person text, shareid text, sharename text, checktime integer, available integer, total integer)" []
     commit db
     password <- getPass'
     questions <- liftM read $ readFile "/home/jcp/ucreditu/questions"
@@ -24,20 +26,20 @@ main = do
         flip catch (\e -> putStrLn $ "Exception happened: " ++ show e) $ do
             putStrLn "Starting session"
             accounts <- runSession defaultSession defaultCaps $ do
-                login "javawizard" questions pass
+                login "javawizard" questions password
                 accounts <- getAccounts
                 logout
                 return accounts
             threadDelay $ 3000*1000
             system "pkill -f firefox"
             putStrLn "Inserting into database"
-            time <- liftM (floor . realToFrac) getPOSIXTime
+            time <- (liftM (floor . realToFrac) getPOSIXTime) :: IO Integer
             forM_ accounts $ \(shareName, _, number, available, total) -> do
-                (run db "insert into balance (person, shareid, sharename, when, available, total) values (?, ?, ?, ?, ?, ?)" $
+                (run db "insert into balance (person, shareid, sharename, checktime, available, total) values (?, ?, ?, ?, ?, ?)" $
                     [toSql "javawizard", toSql $ drop 2 $ dropWhile (/= '-') number, toSql shareName, toSql time,
-                     toSql (read $ filter (flip elem "0123456789") available :: Integer), toSql (read $ filter (flip elem "0123456789") available :: Integer)])
-            commit db 3600*1000*1000
+                     toSql (read $ filter (flip elem "0123456789") available :: Integer), toSql (read $ filter (flip elem "0123456789") total :: Integer)])
+            commit db
             putStrLn "Done"
-        threadDelay (3600*1000*1000)
+        threadDelay (20*1000*1000)
         loop
     loop
