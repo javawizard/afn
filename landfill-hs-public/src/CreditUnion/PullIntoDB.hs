15517fab7a4d069e0a230cb7296c14d5b3e4e857
Alexander Boyd <alex@opengroove.org>
2013-03-03 23:41:55 -0700

    Adding multiple account support to PullIntoDB

--- landfill-hs-public/src/CreditUnion/PullIntoDB.hs
+++ landfill-hs-public/src/CreditUnion/PullIntoDB.hs
@@ -21,25 +21,29 @@ main = do
     run db "create table if not exists balance (person text, shareid text, sharename text, checktime integer, available integer, total integer)" []
     commit db
     password <- getPass'
+    -- [(username, [(q, a), (q, a), (q, a)]), (username, [...]), ...]
+    accountInfos <- liftM read $ readFile "/home/jcp/ucreditu/questions" :: IO [(String, [(String, String)])]
     questions <- liftM read $ readFile "/home/jcp/ucreditu/questions"
     putStrLn "Running."
     let loop = do
         flip catch (\e -> putStrLn $ "Exception happened: " ++ show e) $ do
-            putStrLn "Starting session"
-            accounts <- runSession defaultSession defaultCaps $ do
-                liftIO $ putStrLn "Logging in"
-                login "javawizard" questions password
-                liftIO $ putStrLn "Getting accounts"
-                accounts <- getAccounts
-                logout
-                return accounts
-            threadDelay $ 3000*1000
-            putStrLn "Inserting into database"
             time <- (liftM (floor . realToFrac) getPOSIXTime) :: IO Integer
-            forM_ accounts $ \(shareName, _, number, available, total) -> do
-                (run db "insert into balance (person, shareid, sharename, checktime, available, total) values (?, ?, ?, ?, ?, ?)" $
-                    [toSql "javawizard", toSql $ drop 2 $ dropWhile (/= '-') number, toSql shareName, toSql time,
-                     toSql (read $ filter (flip elem "0123456789") available :: Integer), toSql (read $ filter (flip elem "0123456789") total :: Integer)])
+            putStrLn $ "Starting at time: " ++ show time 
+            forM_ accountInfos $ \(username, questions) -> do
+                putStrLn "Starting session"
+                accounts <- runSession defaultSession defaultCaps $ do
+                    liftIO $ putStrLn $ "Logging into " ++ show username
+                    login username questions password
+                    liftIO $ putStrLn "Getting accounts"
+                    accounts <- getAccounts
+                    logout
+                    return accounts
+                threadDelay $ 3000*1000
+                putStrLn "Inserting into database"
+                forM_ accounts $ \(shareName, _, number, available, total) -> do
+                    (run db "insert into balance (person, shareid, sharename, checktime, available, total) values (?, ?, ?, ?, ?, ?)" $
+                        [toSql username, toSql $ drop 2 $ dropWhile (/= '-') number, toSql shareName, toSql time,
+                         toSql (read $ filter (flip elem "0123456789") available :: Integer), toSql (read $ filter (flip elem "0123456789") total :: Integer)])
             commit db
             putStrLn "Done"
         flip catch (\e -> putStrLn $ "Exception happened while killing firefox: " ++ show e) $ system "pkill -f firefox" >> return ()
