1de84b9989a439901abbf4eb407dfcf91b88da68
Alexander Boyd <alex@opengroove.org>
2013-04-05 01:16:34 -0600

    New bindings system is done, but without support for synthetic bindables. Now I need to think up how to implement proper transactional support for those...

--- afn/python/src/bind2.py
+++ afn/python/src/bind2.py
@@ -6,6 +6,10 @@ SetValue = namedtuple("SetValue", ["value"])
 ModifyKey = namedtuple("ModifyKey", ["key", "value"])
 DeleteKey = namedtuple("DeleteKey", ["key"])
 
+InsertItem = namedtuple("InsertItem", ["index", "item"])
+ReplaceItem = namedtuple("ReplaceItem", ["index", "item"])
+DeleteItem = namedtuple("DeleteItem", ["index"])
+
 class Log(object):
     def __enter__(self):
         return self
@@ -106,8 +110,6 @@ class Dict(Bindable):
         return self._dict
     
     def perform_change(self, change):
-        if not isinstance(change, (ModifyKey, DeleteKey)):
-            raise TypeError("Need a ModifyKey or DeleteKey")
         if isinstance(change, ModifyKey):
             if change.key in self._dict:
                 old = self._dict[change.key]
@@ -118,7 +120,7 @@ class Dict(Bindable):
                 self._dict[change.key] = change.value
                 def undo():
                     del self._dict[change.key]
-        else:
+        elif isinstance(change, DeleteKey):
             if change.key in self._dict:
                 old = self._dict[change.key]
                 del self._dict[change.key]
@@ -126,7 +128,58 @@ class Dict(Bindable):
                     self._dict[change.key] = old
             else: # Delete a non-existent key; no-op
                 undo = lambda: None
+        elif isinstance(change, SetValue):
+            old = self._dict.copy()
+            self._dict.clear()
+            self._dict.update(change.value)
+            def undo():
+                self._dict.clear()
+                self._dict.update(old)
+        else:
+            raise TypeError("Need a ModifyKey or DeleteKey")
         return undo
+
+
+class List(Bindable):
+    def __init__(self):
+        self.binder = Binder(self)
+        self._list = []
+    
+    def get_value(self):
+        return self._list
+    
+    def perform_change(self, change):
+        if isinstance(change, InsertItem):
+            if change.index < 0 or change.index > len(self._list):
+                raise IndexError("Insert index out of range")
+            self._list.insert(change.index, change.item)
+            def undo():
+                del self._list[change.index]
+        elif isinstance(change, ReplaceItem):
+            if change.index < 0 or change.index >= len(self._list):
+                raise IndexError("Replace index out of range")
+            old = self._list[change.index]
+            self._list[change.index] = change.item
+            def undo():
+                self._list[change.index] = old
+        elif isinstance(change, DeleteItem):
+            if change.index < 0 or change.index >= len(self._list):
+                raise IndexError("Delete index out of range")
+            old = self._list[change.index]
+            del self._list[change.index]
+            def undo():
+                self._list.insert(change.index, old)
+        elif isinstance(change, SetValue):
+            old = self._list[:]
+            self._list[:] = []
+            self._list.extend(change.value)
+            def undo():
+                self._list[:] = []
+                self._list.extend(old)
+        else:
+            raise TypeError("Need a list-related change")
+        return undo
+
 
 
 
