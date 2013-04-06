3df29d39a69b9d3f96a3fd46c9d1f860111d6057
Alexander Boyd <alex@opengroove.org>
2013-04-05 21:00:08 -0600

    Generalized _GetSetConnectValue into _PropertyValue, which automatically watches any given named property. I'll be using that for DEntry as well. (I might also name that DTextBox, or alias it, not sure yet.)

--- afn/python/src/bindings/gtk_bind.py
+++ afn/python/src/bindings/gtk_bind.py
@@ -13,38 +13,38 @@ class BlockHandler(object):
     def __exit__(self, *args):
         self.widget.handler_unblock(self.handler)
 
-class _GetSetConnectValue(bind.PyValueMixin, bind.Bindable):
+class _PropertyValue(bind.PyValueMixin, bind.Bindable):
     # Takes a function used to get the current value, a function used to modify
     # the value, and the name of a signal that will fire when the value is
     # changed for us (or None for write-only properties)
-    def __init__(self, widget, getter, setter, signal):
+    def __init__(self, widget, property):
         self.widget = widget
-        self.getter = getter
-        self.setter = setter
-        self.last_value = getter()
-        self.handler = None
-        self.handler = widget.connect(signal, self.handle_signal)
+        self.property = property
+        self.last_value = widget.get_property(property)
+        self.handler = widget.connect("notify::" + property, self.handle_signal)
     
     def get_value(self):
         return self.last_value
     
     def handle_signal(self, *args):
-        new_value = self.getter()
+        # TODO: Make sure we're updating last_value here properly, and same
+        # with perform_change
+        new_value = self.widget.get_property(self.property)
         try:
             self.binder.notify_change(bind.SetValue(new_value))
             self.last_value = new_value
         except: # Validation error
             with BlockHandler(self.widget, self.handler):
-                self.setter(self.last_value)
+                self.widget.set_property(self.property, self.last_value)
     
     def perform_change(self, change):
         old = self.last_value
         with BlockHandler(self.widget, self.handler):
-            self.setter(change.value)
+            self.set_property(self.property, change.value)
             self.last_value = change.value
         def undo():
             with BlockHandler(self.widget, self.handler):
-                self.button.widget.set_state(old)
+                self.set_property(self.property, old)
                 self.last_value = old
         return undo
             
@@ -52,7 +52,7 @@ class _GetSetConnectValue(bind.PyValueMixin, bind.Bindable):
 class DCheckButton(object):
     def __init__(self):
         self.widget = gtk.CheckButton("")
-        self.label = _GetSetConnectValue(self.widget, self.widget.get_label, self.widget.set_label, "notify::label")
-        self.active = _GetSetConnectValue(self.widget, self.widget.get_active, self.widget.set_active, "toggled")
+        self.label = _PropertyValue(self.widget, "label")
+        self.active = _PropertyValue(self.widget, "active")
 
     
