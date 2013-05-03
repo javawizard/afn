67e0fb007665e6f396163a5bab82824b0257f680
Alexander Boyd <alex@opengroove.org>
2013-05-03 02:38:55 -0600

    Prefixed non-public things in stm with _

--- afn/python/src/stm_system/stm.py
+++ afn/python/src/stm_system/stm.py
@@ -2,14 +2,14 @@
 from threading import local as Local, Lock
 from Queue import Queue, Full
 
-class RetryImmediately(BaseException):
+class _RetryImmediately(BaseException):
     pass
 
-class RetryLater(BaseException):
+class _RetryLater(BaseException):
     pass
 
 
-class State(Local):
+class _State(Local):
     def __init__(self):
         self.current = None
     
@@ -18,12 +18,12 @@ class State(Local):
             raise Exception("No current transaction")
         return self.current
 
-stm_state = State()
-global_lock = Lock()
-last_transaction = 0
+_stm_state = _State()
+_global_lock = Lock()
+_last_transaction = 0
 
 
-class Transaction(object):
+class _Transaction(object):
     def __init__(self):
         self.vars = {}
     
@@ -48,41 +48,41 @@ class Transaction(object):
         raise NotImplementedError
 
 
-class BaseTransaction(Transaction):
+class _BaseTransaction(_Transaction):
     def __init__(self):
-        Transaction.__init__(self)
+        _Transaction.__init__(self)
         self.parent = None
         # Store off the transaction id we're starting at, so that we know if
         # things have changed since we started.
-        with global_lock:
-            self.start = last_transaction
+        with _global_lock:
+            self.start = _last_transaction
     
     def get_real_value(self, var):
         # Just check to make sure the variable hasn't been modified since we
-        # started (and raise RetryImmediately if it has), then return its real
+        # started (and raise _RetryImmediately if it has), then return its real
         # value.
-        with global_lock:
+        with _global_lock:
             if var.modified > self.start:
-                raise RetryImmediately
+                raise _RetryImmediately
             return var.real_value
     
     def run(self, function):
-        global last_transaction
+        global _last_transaction
         try:
             # First we actually run the transaction.
             result = function()
-            # Transaction appears to have run successfully. Now we need to make
+            # _Transaction appears to have run successfully. Now we need to make
             # sure nothing it used changed in the mean time.
-            with global_lock:
+            with _global_lock:
                 for var in self.vars:
                     if var.modified > self.start:
                         # Var was modified since we started, so we need to
                         # retry against its new value.
-                        raise RetryImmediately
+                        raise _RetryImmediately
                 # Nothing changed, so we're good to commit. First we make
                 # ourselves a new id.
-                last_transaction += 1
-                modified = last_transaction
+                _last_transaction += 1
+                modified = _last_transaction
                 # Then we update the real values of all of the TVars. Note that
                 # TVar.update_real_value takes care of notifying the TVar's
                 # queues for us.
@@ -90,16 +90,16 @@ class BaseTransaction(Transaction):
                     var.update_real_value(value, modified)
                 # And we're done!
                 return result
-        except RetryLater:
+        except _RetryLater:
             # Received a retry request that made it all the way up to the top.
             # First, check to see if any of the variables we've accessed have
             # been modified since we started, which could change whether or not
             # we need to retry.
-            with global_lock:
+            with _global_lock:
                 for var in self.vars:
                     if var.modified > self.start:
                         # Yep, one changed. Retry immediately.
-                        raise RetryImmediately
+                        raise _RetryImmediately
                 # Nope, none of them have changed. So now we create a queue,
                 # then add it to all of the vars we need to watch.
                 q = Queue(1)
@@ -109,16 +109,16 @@ class BaseTransaction(Transaction):
             q.get()
             # One of the vars was modified. Now we go remove ourselves from
             # the vars' queues.
-            with global_lock:
+            with _global_lock:
                 for var in self.vars:
                     var.queues.remove(q)
             # And then we retry immediately.
-            raise RetryImmediately
+            raise _RetryImmediately
 
 
-class NestedTransaction(Transaction):
+class _NestedTransaction(_Transaction):
     def __init__(self, parent):
-        Transaction.__init__(self)
+        _Transaction.__init__(self)
         self.parent = parent
     
     def get_real_value(self, var):
@@ -127,7 +127,7 @@ class NestedTransaction(Transaction):
     
     def run(self, function):
         # Run the function, then (if it didn't throw any exceptions;
-        # RetryImmediately, RetryLater, or otherwise) copy our values into
+        # _RetryImmediately, _RetryLater, or otherwise) copy our values into
         # our parent.
         result = function()
         for var, value in self.vars.iteritems():
@@ -143,11 +143,11 @@ class TVar(object):
     
     def get(self):
         # Ask the current transaction for our value.
-        return stm_state.get_current().get_value(self)
+        return _stm_state.get_current().get_value(self)
     
     def set(self, value):
         # Set the specified value into the current transaction.
-        stm_state.get_current().set_value(self, value)
+        _stm_state.get_current().set_value(self, value)
     
     value = property(get, set)
     
@@ -171,48 +171,48 @@ class TVar(object):
 
 def atomically(function):
     while True:
-        # If we have no current transaction, create a BaseTransaction.
-        # Otherwise, create a NestedTransaction with the current one as its
+        # If we have no current transaction, create a _BaseTransaction.
+        # Otherwise, create a _NestedTransaction with the current one as its
         # parent.
-        if stm_state.current:
-            transaction = NestedTransaction(stm_state.current)
+        if _stm_state.current:
+            transaction = _NestedTransaction(_stm_state.current)
         else:
-            transaction = BaseTransaction()
+            transaction = _BaseTransaction()
         # Then set it as the current transaction
-        stm_state.current = transaction
-        # Then run the transaction. BaseTransaction's implementation takes care
-        # of catching RetryLater and blocking until one of the vars we read is
-        # modified, then converting it into a RetryImmediately exception.
+        _stm_state.current = transaction
+        # Then run the transaction. _BaseTransaction's implementation takes care
+        # of catching _RetryLater and blocking until one of the vars we read is
+        # modified, then converting it into a _RetryImmediately exception.
         try:
             return transaction.run(function)
-        except RetryImmediately:
-            # We were asked to retry immediately. If we're a BaseTransaction,
-            # just continue. If we're a NestedTransaction, propagate the
+        except _RetryImmediately:
+            # We were asked to retry immediately. If we're a _BaseTransaction,
+            # just continue. If we're a _NestedTransaction, propagate the
             # exception up. TODO: Figure out a way to move this logic into
-            # individual methods on Transaction that BaseTransaction and
-            # NestedTransaction can override accordingly.
-            if isinstance(transaction, BaseTransaction):
+            # individual methods on _Transaction that _BaseTransaction and
+            # _NestedTransaction can override accordingly.
+            if isinstance(transaction, _BaseTransaction):
                 continue
             else:
                 raise
         finally:
             # Before we go, restore the current transaction to whatever it was
             # before we started.
-            stm_state.current = transaction.parent
+            _stm_state.current = transaction.parent
 
 
 def retry():
-    raise RetryLater
+    raise _RetryLater
 
 
 def or_else(*args):
     for function in args:
         # Try to run each function in sequence, in its own transaction so that
-        # if it raises RetryLater (or any other exception) its effects will be
+        # if it raises _RetryLater (or any other exception) its effects will be
         # undone.
         try:
             return atomically(function)
-        except RetryLater:
+        except _RetryLater:
             # Requested a retry, so move on to the next alternative
             pass
     # All of the alternatives retried, so retry ourselves.
