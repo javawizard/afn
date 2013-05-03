76ad56f52b6e0f67a3e83a57969ef6d5386b843a
Alexander Boyd <alex@opengroove.org>
2013-05-03 01:22:00 -0600

    Rewrote BroadcastQueue in terms of TObject, which offers significantly increased readability at the cost of (slightly) worse (but still constant) performance

--- afn/python/src/stm_system/datatypes/broadcastqueue.py
+++ afn/python/src/stm_system/datatypes/broadcastqueue.py
@@ -1,5 +1,6 @@
 
 from stm_system import stm
+from stm_system.datatypes import tobject
 
 class Empty(Exception):
     pass
@@ -10,28 +11,28 @@ class _Item(object):
         self.value = value
         self.next = stm.TVar()
 
-class BroadcastQueue(object):
+class BroadcastQueue(tobject.TObject):
     def __init__(self):
-        self._var = stm.TVar(stm.TVar(None))
+        self._var = stm.TVar(None)
     
     def put(self, value):
         item = _Item(value)
-        self._var.get().set(item)
-        self._var.set(item.next)
+        self._var.set(item)
+        self._var = item.next
     
     def new_endpoint(self):
-        return _BroadcastEndpoint(self._var.get())
+        return _BroadcastEndpoint(self._var)
 
 
-class _BroadcastEndpoint(object):
+class _BroadcastEndpoint(tobject.TObject):
     def __init__(self, var):
-        self._var = stm.TVar(var)
+        self._var = var
     
     def get(self):
-        if self._var.get().get() is None:
+        if self._var.get() is None:
             stm.retry()
         else:
-            item = self._var.get().get()
-            self._var.set(item.next)
+            item = self._var.get()
+            self._var = item.next
             return item.value
         
