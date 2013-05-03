64356d9a70312130089cc681a2d6d8bb54eeb738
Alexander Boyd <alex@opengroove.org>
2013-05-02 20:26:48 -0600

    Factoring things into separate packages

--- afn/python/src/stm_system/datatypes.py
+++ afn/python/src/stm_system/datatypes.py
@@ -1,5 +1,6 @@
 
 from stm_system import stm
+from stm_system.avl import *
 
 class Empty(Exception):
     pass
@@ -34,5 +35,65 @@ class _BroadcastEndpoint(object):
             item = self._var.get().get()
             self._var.set(item.next)
             return item.value
+        
+def list_insert(node, index, value):
+    if isinstance(node, Empty):
+        # Empty, so just return a node with our value. TODO: Could check that
+        # the index is == 0; if it's greater, then the original insertion index
+        # is greater than the length of the list plus one, which doesn't make
+        # sense.
+        return Node(empty, value, empty)
+    if index < node.left.weight: # Go left
+        return balance(Node(list_insert(node.left, index, value), node.value, node.right))
+    elif index > node.left.weight: # Go right, but subtract left.weight + 1
+        # from the index
+        return balance(Node(node.left, node.value, list_insert(node.right, index - 1 - node.left.weight, value)))
+    else: # Node's supposed to be inserted here
+        return balance(Node(empty, value, node))
+
+
+def list_replace(node, index, value):
+    if isinstance(node, Empty):
+        raise IndexError
+    if index < node.left.weight:
+        return Node(list_replace(node.left, index, value), node.value, node.right)
+    elif index > node.left.weight:
+        return Node(node.left, node.value, list_replace(node.right, index - node.left.weight - 1, value))
+    else:
+        return Node(node.left, value, node.right)
+
+
+def list_get(node, index):
+    if isinstance(node, Empty):
+        raise IndexError
+    if index < node.left.weight:
+        return list_get(node.left, index)
+    elif index > node.left.weight:
+        return list_get(node.right, index - node.left.weight - 1)
+    else:
+        return node.value
+
+
+class TList(object):
+    def __init__(self):
+        self.var = stm.TVar(empty)
+    
+    def __getitem__(self, index):
+        return list_get(self.var.get(), index)
+    
+    def __setitem__(self, index, value):
+        self.var.set(list_replace(self.var.get(), index, value))
+    
+    def __delitem__(self, index):
+        raise NotImplementedError
+    
+    def __len__(self):
+        return self.var.get().weight
+    
+    def insert(self, index, value):
+        self.var.set(list_insert(self.var.get(), index, value))
+    
+    def __str__(self):
+        return "TList(%r)" % list(self)
 
 
