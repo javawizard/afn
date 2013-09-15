b01f9eeee97cec46320905ce19ac8478931d740c
Alexander Boyd <alex@opengroove.org>
2013-09-14 21:20:28 -0600

    Merge tdict, tlist, and tobject into a single module, datatypes. I'll be merging broadcastqueue in shortly.

--- afn/python/src/stm/datatypes.py
+++ afn/python/src/stm/datatypes.py
@@ -1,6 +1,12 @@
+"""
+Some useful data types built on top of the STM system.
+
+This module provides several transactional data types built on top of the
+primitives provided by the STM module.
+"""
 
 from afn import ttftree
-from collections import MutableSequence
+from collections import MutableSequence, MutableMapping
 import stm
 
 class TList(MutableSequence):
@@ -156,3 +162,113 @@ class TList(MutableSequence):
     __repr__ = __str__
 
 
+class TDict(MutableMapping):
+    """
+    UPDATE: This now uses 2-3 finger trees. Update accordingly.
+    
+    A transactional dictionary.
+    
+    Internally, transactional dicts are maintained with a single TVar holding a
+    copy-on-write binary tree annotated with dict keys. Insertion (and
+    appending), removal, and lookup are therefore all O(log n) operations.
+    len() is O(1), as is iter(), iterkeys(), iteritems(), and itervalues().
+    
+    One nice property of using a copy-on-write binary tree is iteration: the
+    iterator returned from iter(tdict) is a snapshot of the dict's keys at that
+    point in time. The dict can therefore be safely modified during iteration,
+    without affecting the keys produced by the iteration. The same is, of
+    course, true of iterkeys, iteritems, and itervalues.
+    
+    All of TDict's functions must be called within an STM transaction, with the
+    exception of __str__/__repr__, which, for the sake of convenience,
+    wrap themselves in a call to stm.atomically() internally. 
+    """
+    def __init__(self, initial_values=None):
+        self.var = stm.TVar(ttftree.Empty(ttftree.CompoundMeasure(ttftree.MeasureItemCount(), ttftree.TranslateMeasure(lambda (k, v): k, ttftree.MeasureLastItem()))))
+        if initial_values:
+            # Optimize to O(1) if we're cloning another TDict
+            if isinstance(initial_values, TDict):
+                self.var.set(initial_values.var.get())
+            # Initializing from another dict-like object
+            elif hasattr(initial_values, "keys"):
+                for k in initial_values.keys():
+                    self[k] = initial_values[k]
+            # Initializing from a sequence of 2-tuples
+            else:
+                for k, v in initial_values:
+                    self[k] = v
+    
+    def __getitem__(self, key):
+        left, right = self.var.get().partition(lambda (i, k): k >= key)
+        if right.is_empty or right.get_first()[0] != key:
+            raise KeyError(key)
+        return right.get_first()[1]
+    
+    def __setitem__(self, key, value):
+        left, right = self.var.get().partition(lambda (i, k): k >= key)
+        if not right.is_empty and right.get_first()[0] == key:
+            right = right.without_first()
+        self.var.set(left.add_last((key, value)).append(right))
+    
+    def __delitem__(self, key):
+        left, right = self.var.get().partition(lambda (i, k): k >= key)
+        if right.is_empty or right.get_first()[0] != key:
+            raise KeyError(key)
+        self.var.set(left.append(right.without_first()))
+    
+    def __iter__(self):
+        for k, _ in ttftree.value_iterator(self.var.get()):
+            yield k
+    
+    def __len__(self):
+        length, _ = self.var.get().annotation
+        return length
+    
+    def iterkeys(self):
+        return iter(self)
+    
+    def keys(self):
+        return list(self.iterkeys())
+    
+    def itervalues(self):
+        for _, v in ttftree.value_iterator(self.var.get()):
+            yield v
+    
+    def values(self):
+        return list(self.itervalues())
+    
+    def iteritems(self):
+        return ttftree.value_iterator(self.var.get())
+    
+    def items(self):
+        return list(self.iteritems())
+    
+    def __str__(self):
+        return "TDict(%r)" % stm.atomically(lambda: dict(self))
+    
+    __repr__ = __str__
+
+
+class TObject(object):
+    """
+    An abstract class that causes all of its subclass's attributes to be backed
+    by a TDict. This results in the subclass's attributes being transactional,
+    so that they can be modified during a transaction without having to
+    explicitly wrap all of them with TVars.
+    """
+    def __init__(self):
+        object.__setattr__(self, "_tobject_dict", TDict())
+    
+    def __getattr__(self, name):
+        if name == "_tobject_dict":
+            return object.__getattribute__(self, name)
+        return self._tobject_dict[name]
+    
+    def __setattr__(self, name, value):
+        self._tobject_dict[name] = value
+    
+    def __delattr__(self, name):
+        del self._tobject_dict[name]
+    
+    def __dir__(self):
+        return self._tobject_dict.keys()
