e39836e6b3aad53482c52c9f87e8bafc42379a59
Alexander Boyd <alex@opengroove.org>
2013-09-14 21:28:18 -0600

    Merged tset into datatypes. Incidentally, I'm hoping to rewrite tset in terms of ttftree soon, but I still need to figure out how to properly (and efficiently) handle weak sets.

--- afn/python/src/stm/datatypes.py
+++ afn/python/src/stm/datatypes.py
@@ -274,6 +274,42 @@ class TObject(object):
         return self._tobject_dict.keys()
 
 
+class TSet(MutableSet):
+    """
+    A transactional mutable set.
+    
+    Right now, this actually just uses an underlying Python set which it copies
+    on every write, so it's woefully inefficient. I'll be changing it to use
+    the AVL trees that TList and TDict use soon, but I need to figure out how
+    to properly implement weak sets in terms of them first. (I could always
+    just have weak nodes pointing to dereferenced objects collected only during
+    iteration, but then the iterators can't be completely isolated from the set
+    itself, so that needs some thought.)
+    """
+    def __init__(self, initial_items=set(), backing_type=set):
+        self.backing_type = backing_type
+        self._var = stm.TVar(backing_type(initial_items))
+    
+    def __contains__(self, item):
+        return self._var.get().__contains__(item)
+    
+    def __iter__(self):
+        return self._var.get().__iter__()
+    
+    def __len__(self):
+        return self._var.get().__len__()
+    
+    def add(self, item):
+        new_set = self.backing_type(self._var.get())
+        new_set.add(item)
+        self._var.set(new_set)
+    
+    def discard(self, item):
+        new_set = self.backing_type(self._var.get())
+        new_set.discard(item)
+        self._var.set(new_set)
+
+
 class Empty(Exception):
     """
     Exception thrown from BroadcastEndpoint.get() when block=False is passed
